d8be63db11587c509429253174c7d88797a71a43
LeHieu <Xavan912@gmail.com>
2020-07-30 10:52:17 +0700

    Create data.js
    Them du lieu demo danh sach cac thong bao

---
+++ src/elements/ListBar/data.js
@@ -0,0 +1,46 @@
+// import uuid from 'uuid/v1';
+
+export default [
+    {
+      type: "Người dùng",
+      items: [
+        {
+          id      : 1,
+          name    : "Lê Viết Dũng",
+          avatar  : "/images/avatars/avatar_1.png",
+          topic   : "Ali Connors",
+          content : "I'll be in your neighborhood doing errands this..",
+          time    : "5 phút trước"
+        },
+        {
+          id      : 2,
+          name    : "Nguyễn Thế Cường",
+          avatar  : "/images/avatars/avatar_2.png",
+          topic   : "to Scott, Alex, Jennifer",
+          content : "Wish I could come, but I'm out of town this…",
+          time    : "15 phút trước"
+        },
+        {
+          id      : 3,
+          name    : "Trần Đơn",
+          avatar  : "/images/avatars/avatar_3.png",
+          topic   : "Sandra Adams",
+          content : "Do you have Paris recommendations? Have you ever…",
+          time    : "1 giờ trước"
+        },
+      ]
+    },
+    {
+      type: "Hệ thống",
+      items: [
+        {
+          id      : 1,
+          name    : "Dịch vụ email",
+          avatar  : "/images/avatars/avatar_4.png",
+          topic   : "Trả lời comment",
+          content : "Hãy trả lời các câu hỏi cho độc giả của bạn..",
+          time    : "2 giờ trước"
+        }
+      ]
+    }
+  ];
