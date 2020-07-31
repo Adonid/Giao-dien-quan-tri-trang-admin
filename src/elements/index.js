e3159e6329c531cb93636d6e09751d3d9fc30e05
LeHieu <Xavan912@gmail.com>
2020-07-31 16:50:33 +0700

    Update index.js
    Them component ListNotifys vao dung vi tri forder

--- src/elements/index.js
+++ src/elements/index.js
@@ -1,4 +1,3 @@
 export { default as Buttons} from './Buttons';
 export { default as BackDrop} from './BackDrop';
 export { default as Skeletons} from './Skeletons';
-export { default as ListBar} from './ListBar';
