4a8e31a018f42d7fd8f955c83c61c1390c0f47b0
LeHieu <Xavan912@gmail.com>
2020-07-31 23:08:25 +0700

    Update AlertNotify.js
    Thu dua du lieu vao props

--- src/layouts/Main/components/Topbar/components/ListNotifys/AlertNotify.js
+++ src/layouts/Main/components/Topbar/components/ListNotifys/AlertNotify.js
@@ -17,26 +17,30 @@ import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
+import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+
+import Tooltip from '@material-ui/core/Tooltip';
 
 const useStyles = makeStyles((theme) => ({
     root: {
       maxWidth: 345,
     },
-    expand: {
-      transform: 'rotate(0deg)',
-      marginLeft: 'auto',
-      transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-      }),
-    },
-    expandOpen: {
-      transform: 'rotate(180deg)',
+    titleDialog: {
+      fontSize: 18,
+      fontWeight: 500
     },
     avatar: {
       backgroundColor: red[500],
     },
+    colorDanger: {
+      color: '#f44336',
+      padding: theme.spacing(0.75)
+    },
+    colorSuccess: {
+      color: '#43a047',
+      padding: theme.spacing(0.75)
+    }
   }));
 
 const AlertNotify = props => {
@@ -55,6 +59,19 @@ const AlertNotify = props => {
         setOpen(false);
     };
 
+    const handleReply = () => {
+        setOpen(false);
+        // go to page target
+    };
+
+    const handleDelete = () => {
+      
+    }
+
+    const handleMark = () => {
+
+    }
+
   return (
     <div>
       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
@@ -67,44 +84,59 @@ const AlertNotify = props => {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+        <DialogTitle id="alert-dialog-title">
+          <Typography className={classes.titleDialog}>{"Use Google's location service?"}</Typography>
+        </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
 
             <Card className={classes.root}>
                 <CardHeader
                     avatar={
-                    <Avatar aria-label="recipe" className={classes.avatar}>
-                        R
-                    </Avatar>
+                    !props.avatar ? <Avatar aria-label="recipe" className={ classes.avatar }>{'props.name'.substring(0,1)}</Avatar> : <Avatar alt={ props.name } src={ props.avatar } />
                     }
-                    title="Shrimp and Chorizo Paella"
-                    subheader="September 14, 2016"
+                    title={<Typography variant="h6" component="p"> { props.name } </Typography>}
+                    subheader={<Typography variant="caption">{ props.time }</Typography>}
                 />
                 <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                    This impressive paella is a perfect party dish and a fun meal to cook together with your
-                    guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                    <Typography variant="h6" component="span">
+                      { props.topic }
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="span">
+                      { "— " + props.content }
                     </Typography>
                 </CardContent>
                 <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                    </IconButton>
-                    <IconButton aria-label="share">
-                    <ShareIcon />
-                    </IconButton>
+                    
+                    <Tooltip title="Xóa thông báo này">
+                      <IconButton 
+                        className={classes.colorDanger} 
+                        aria-label="Xóa thông báo này" 
+                        onClick={ handleDelete } >
+                          <HighlightOffIcon />
+                      </IconButton>
+                    </Tooltip>
+                    
+                    <Tooltip title="Đánh dấu là đã đọc">
+                      <IconButton 
+                        className={ props.isReaded ? classes.colorSuccess : null } 
+                        aria-label={ props.isReaded ? "Đánh dấu là đã đọc" : "Đánh dấu là chưa đọc" }
+                        onClick={ handleMark }
+                        >
+                          <AssignmentTurnedInIcon />
+                      </IconButton>
+                    </Tooltip>
                 </CardActions>
             </Card>
 
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Disagree
+          <Button onClick={handleReply} color="primary" autoFocus>
+            Phản hồi
           </Button>
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Agree
+          <Button onClick={handleClose} color="secondary">
+            Đóng
           </Button>
         </DialogActions>
       </Dialog>
