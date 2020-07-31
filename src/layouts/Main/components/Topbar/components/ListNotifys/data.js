066dc4cd5cd867e39d682e7280b2643324cf92d3
LeHieu <Xavan912@gmail.com>
2020-07-31 16:52:41 +0700

    Create data.js
    Di chuyen du lieu toi

---
+++ src/layouts/Main/components/Topbar/components/ListNotifys/data.js
@@ -0,0 +1,88 @@
+// import uuid from 'uuid/v1';
+
+export default [
+    {
+      type: "Người dùng",
+      ref: 1,
+      items: [
+        {
+          id      : 1,
+          name    : "Lê Viết Dũng",
+          avatar  : "/images/avatars/avatar_1.png",
+          topic   : "Ali Connors",
+          content : "I'll be in your neighborhood doing errands this..",
+          time    : "5 phút trước",
+          isRead  : false
+        },
+        {
+          id      : 2,
+          name    : "Nguyễn Thế Cường",
+          avatar  : "/images/avatars/avatar_2.png",
+          topic   : "to Scott, Alex, Jennifer",
+          content : "Wish I could come, but I'm out of town this…",
+          time    : "15 phút trước",
+          isRead  : false
+        },
+        {
+          id      : 3,
+          name    : "Trần Đơn",
+          avatar  : "/images/avatars/avatar_3.png",
+          topic   : "Sandra Adams",
+          content : "Do you have Paris recommendations? Have you ever…",
+          time    : "1 giờ trước",
+          isRead  : true
+        },
+      ]
+    },
+    {
+      type: "Hệ thống bình luận",
+      ref: 2,
+      items: [
+        {
+          id      : 1,
+          name    : "Nguyễn Chiến",
+          avatar  : "/images/avatars/avatar_5.png",
+          topic   : "Du lịch Hà Thành không thể bỏ qua 5 địa điểm này",
+          content : "Khi nào tour du lịch Hà Nội-Vinh mở bạn ơi!",
+          time    : "19 phút trước",
+          isRead  : false
+        },
+        {
+          id      : 2,
+          name    : "Nguyễn Chiến",
+          avatar  : "/images/avatars/avatar_6.png",
+          topic   : "Du lịch Hà Thành không thể bỏ qua 5 địa điểm này",
+          content : "Khi nào tour du lịch Hà Nội-Vinh mở bạn ơi!",
+          time    : "19 phút trước",
+          isRead  : false
+        },
+        {
+          id      : 3,
+          name    : "Nguyễn Chiến",
+          avatar  : "/images/avatars/avatar_7.png",
+          topic   : "Du lịch Hà Thành không thể bỏ qua 5 địa điểm này",
+          content : "Khi nào tour du lịch Hà Nội-Vinh mở bạn ơi!",
+          time    : "19 phút trước",
+          isRead  : true
+        },
+        {
+          id      : 4,
+          name    : "Nguyễn Chiến",
+          avatar  : "/images/avatars/avatar_8.png",
+          topic   : "Du lịch Hà Thành không thể bỏ qua 5 địa điểm này",
+          content : "Khi nào tour du lịch Hà Nội-Vinh mở bạn ơi!",
+          time    : "19 phút trước",
+          isRead  : false
+        },
+        {
+          id      : 5,
+          name    : "Nguyễn Chiến",
+          avatar  : "/images/avatars/avatar_9.png",
+          topic   : "Du lịch Hà Thành không thể bỏ qua 5 địa điểm này",
+          content : "Khi nào tour du lịch Hà Nội-Vinh mở bạn ơi!",
+          time    : "19 phút trước",
+          isRead  : true
+        }
+      ]
+    }
+  ];
