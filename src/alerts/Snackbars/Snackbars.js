f7e637c9047a97850849f7b088f748f97de74509
LeHieu <Xavan912@gmail.com>
2020-08-03 15:30:19 +0700

    Update Snackbars.js
    Config lai SnackBars show thong bao

--- src/alerts/Snackbars/Snackbars.js
+++ src/alerts/Snackbars/Snackbars.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
@@ -18,33 +19,42 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Snackbars = props => {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const { data, ...rest} = props;
 
-  const handleClick = () => {
-    setOpen(true);
-  };
+  const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+  
+  /** Khong xuat hien dialog lan dau khi chay. Khoi tao cac useState ve tt ban dau */
+  const firstUpdate = useRef(true);
+    useLayoutEffect (() => {
+      if (firstUpdate.current) {
+        firstUpdate.current = false;
+        return;
+      }
+      setOpen(true);
+    },[data])
+  /** End */
 
   const handleClose = (event, reason) => {
     if (reason === 'clickaway') {
       return;
     }
-
     setOpen(false);
   };
 
   return (
     <div className={classes.root}>
-      <Button variant="outlined" onClick={handleClick}>
-        Open success snackbar
-      </Button>
-      <Snackbar open={open} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} autoHideDuration={5000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity={props.type}>
-          {props.content}
+      <Snackbar open={ open } anchorOrigin={{ vertical: 'top', horizontal: 'right' }} autoHideDuration={5000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={ data.type }>
+          { data.content }
         </Alert>
       </Snackbar>
     </div>
   );
+}
+
+Snackbars.propTypes = {
+  data: PropTypes.object
 }
 
 export default Snackbars;
