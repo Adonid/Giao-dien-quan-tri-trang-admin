383af210d82477471981fb5107c9e40aea2f2aa8
LeHieu <Xavan912@gmail.com>
2020-08-04 11:19:39 +0700

    Update index.js
    Them duong dan cho Provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
 import App from './App';
+import { Provider } from 'react-redux'
 import Store from './redux';
 
 ReactDOM.render(
