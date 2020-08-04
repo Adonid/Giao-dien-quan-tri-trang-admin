67076efa6a4c4ab1fec121c7fcce3f12dd2f4d6e
LeHieu <Xavan912@gmail.com>
2020-08-04 17:41:07 +0700

    Update SnackBarReducer.js
    Lay du lieu ref va id , name phuc vu api

--- src/redux/reducers/SnackBarReducer/SnackBarReducer.js
+++ src/redux/reducers/SnackBarReducer/SnackBarReducer.js
@@ -6,8 +6,16 @@ const dataSnackBar = {
 const SnackBarReducer = (state = dataSnackBar, actions) => {
 
     switch (actions.type) {
-        case 'OPEN_SNACKBAR':
-            state = actions.data;
+        // Thuc ra day phai goi API de luu lai feedback, sau do moi dua ra thong bao. day la vd sau khi da goi api xong
+        case 'REPLY':
+            const dataReader = actions.data;
+            /** api reply o day */
+             // ref, id, name
+            /** end */
+            // Neu call api success
+            state = { ...state, type: "success", content: `Đã trả lời vào bình luận của ${dataReader.name} !` }
+            // Neu call api fail
+            // state = { ...state, type: "warning", content: `Đã có lỗi! Chưa trả lời được vào bình luận của ${dataReader.name} !` }
             return state
 
         default:
