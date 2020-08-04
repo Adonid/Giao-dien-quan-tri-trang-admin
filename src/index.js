4f2600c567e407804fd1bb93b463c40d1d415b2f
LeHieu <Xavan912@gmail.com>
2020-08-04 08:02:36 +0700

    Update index.js
    Dua store vao ung dung

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
 import App from './App';
+import { Store } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={ Store }>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+);
 
 serviceWorker.unregister();
