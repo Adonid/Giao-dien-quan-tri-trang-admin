528bff82f078109b962459c7cb671914e0031fae
LeHieu <Xavan912@gmail.com>
2020-08-04 08:02:47 +0700

    Update Snackbars.js
    Di chuyen ra ngang hang voi forder goc. vi day la thong bao toan cuc

--- src/alerts/Snackbars/Snackbars.js
+++ src/alerts/Snackbars/Snackbars.js
@@ -1,6 +1,5 @@
 import React, { useLayoutEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 import { makeStyles } from '@material-ui/core/styles';
