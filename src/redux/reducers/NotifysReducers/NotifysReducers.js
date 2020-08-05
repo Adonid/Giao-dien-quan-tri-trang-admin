e8946bcc9cfd0713f939131b79c98f1150cf11ba
LeHieu <Xavan912@gmail.com>
2020-08-05 10:00:46 +0700

    Update NotifysReducers.js
    Gop du lieu thanh 1 state dataNotify

--- src/redux/reducers/NotifysReducers/NotifysReducers.js
+++ src/redux/reducers/NotifysReducers/NotifysReducers.js
@@ -1,6 +1,15 @@
 const dataNotify = {
-    type    : "info",
-    content : "Hello Word"
+    alert: {
+        type    : "info",
+        content : "Hello Word"
+    },
+    dataNotifys: {
+
+    }
+}
+
+const datax = {
+    hello: 'xin chao'
 }
 
 const SnackBarReducer = (state = dataNotify, actions) => {
@@ -13,15 +22,41 @@ const SnackBarReducer = (state = dataNotify, actions) => {
              // ref, id, name
             /** end */
             // Neu call api success
-            state = { ...state, type: "success", content: `Đã trả lời vào bình luận của ${dataReader.name} !` }
+            state = { ...state, alert: { ...state.alert, type: "success", content: `Đã trả lời vào bình luận của ${dataReader.name} !` } }
             // Neu call api fail
-            // state = { ...state, type: "warning", content: `Đã có lỗi! Chưa trả lời được vào bình luận của ${dataReader.name} !` }
+            // state = { ...state, alert: { ...state.alert, type: "warning", content: `Đã có lỗi! Chưa trả lời được vào bình luận của ${dataReader.name} !` } }
             return state
+
         case 'MARKALL':
             /** api danh dau tat ca la da doc */
             
             /** end */
-            break;
+            console.log('da mark all');
+            return state;
+
+        case 'DELETEALL':
+            /** api xoa tat ca thong bao */
+            
+            /** end */
+            console.log('da xoa het thong bao');
+            return state;
+        
+        case 'DELETENOTE':
+            /** api xoa 1 thong bao */
+            
+            /** end */
+            const note = actions.note;
+            state = { ...state, alert: { ...state.alert, type: "info", content: `Đã xóa 1 thông báo từ ${note.name} !` } }
+            return state;
+        
+        case 'MARKNOTE':
+            /** api danh dau 1 thong bao la da doc */
+            
+            /** end */
+            const not = actions.not;
+            console.log(not);
+            return state;
+
         default:
             return state
     }
