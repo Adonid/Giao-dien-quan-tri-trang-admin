97db68389808177973cb1c9357f517bc516216e5
LeHieu <Xavan912@gmail.com>
2020-08-06 12:05:17 +0700

    Update UsersToolbar.js
    Them icon

--- src/views/UserList/components/UsersToolbar/UsersToolbar.js
+++ src/views/UserList/components/UsersToolbar/UsersToolbar.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/styles';
 import { Button } from '@material-ui/core';
-
+import CloudUploadOutlinedIcon from '@material-ui/icons/CloudUploadOutlined';
+import CloudDownloadOutlinedIcon from '@material-ui/icons/CloudDownloadOutlined';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import { SearchInput } from 'components';
 
 const useStyles = makeStyles(theme => ({
@@ -40,13 +42,13 @@ const UsersToolbar = props => {
     >
       <div className={classes.row}>
         <span className={classes.spacer} />
-        <Button className={classes.importButton}>Import</Button>
-        <Button className={classes.exportButton}>Export</Button>
+        <Button className={classes.importButton}><CloudUploadOutlinedIcon/> Import</Button>
+        <Button className={classes.exportButton}><CloudDownloadOutlinedIcon/> Export</Button>
         <Button
           color="primary"
           variant="contained"
         >
-          Add user
+          <AddCircleOutlineIcon/> Add user
         </Button>
       </div>
       <div className={classes.row}>
