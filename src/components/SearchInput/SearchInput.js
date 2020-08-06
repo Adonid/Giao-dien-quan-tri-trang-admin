a3524a374315059df4706ec88030ddf4d3e7e0cf
LeHieu <Xavan912@gmail.com>
2020-08-06 19:07:34 +0700

    Update SearchInput.js
    Sua lai cho khop voi useTable

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -2,26 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/styles';
-import { Paper, Input } from '@material-ui/core';
+import { Paper, TextField  } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    borderRadius: '4px',
     alignItems: 'center',
-    padding: theme.spacing(1),
     display: 'flex',
     flexBasis: 420
-  },
-  icon: {
-    marginRight: theme.spacing(1),
-    color: theme.palette.text.secondary
   },
   input: {
     flexGrow: 1,
     fontSize: '14px',
     lineHeight: '16px',
     letterSpacing: '-0.05px'
+  },
+  colorSeach: {
+    color: '#546e7a'
   }
 }));
 
@@ -36,13 +34,23 @@ const SearchInput = props => {
       className={clsx(classes.root, className)}
       style={style}
     >
-      <SearchIcon className={classes.icon} />
-      <Input
-        {...rest}
-        className={classes.input}
-        disableUnderline
-        onChange={onChange}
-      />
+      {/* <SearchIcon className={classes.icon} /> */}
+      <TextField 
+          {...rest}
+          className={classes.input}
+          disableUnderline
+          onChange={onChange}
+          id="outlined-helperText"
+          placeholder="Tìm kiếm"
+          variant="outlined"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon className={classes.colorSeach} />
+              </InputAdornment>
+            ),
+          }}
+        />
     </Paper>
   );
 };
