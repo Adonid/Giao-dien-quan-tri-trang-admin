a5398a2bbc75150693dd527f1790b7ec0a77cefd
LeHieu <Xavan912@gmail.com>
2020-08-08 21:44:29 +0700

    Update UserList.js
    Dua MockData sang component UserTable

--- src/views/UserList/UserList.js
+++ src/views/UserList/UserList.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 
 import { UsersToolbar, UsersTable } from './components';
-import mockData from './data';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,13 +15,11 @@ const useStyles = makeStyles(theme => ({
 const UserList = () => {
   const classes = useStyles();
 
-  const [users] = useState(mockData);
-
   return (
     <div className={classes.root}>
       <UsersToolbar />
       <div className={classes.content}>
-        <UsersTable users={users} />
+        <UsersTable />
       </div>
     </div>
   );
