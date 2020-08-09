aba4cb50f1304dcebb3235f0c751f35aa8edf584
LeHieu <Xavan912@gmail.com>
2020-08-09 13:17:39 +0700

    Update index.js
    Them component Modal khi them moi nguoi dung

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as SearchInput } from './SearchInput';
 export { default as StatusBullet } from './StatusBullet';
 export { default as RouteWithLayout } from './RouteWithLayout';
 export { default as SelectInput } from './SelectInput';
+export { default as FormAddUser } from './FormAddUser';
