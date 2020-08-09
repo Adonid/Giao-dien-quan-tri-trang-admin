7c9dc8b2755a0f542c90a534d976bbbb7e1153c6
LeHieu <Xavan912@gmail.com>
2020-08-09 17:14:49 +0700

    Update Store.js
    Them reducer Thao tac voi du lieu nguoi dung
    + Truoc het la mo modal them moi nguoi dung

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,11 +1,13 @@
 import {
-    NotifysReducers as Notifys
+    NotifysReducers as Notifys,
+    UserReducers as User
 } from './reducers';
 
 const redux = require('redux');
 
 var allReducers = redux.combineReducers({
-    dataNotifys: Notifys
+    dataNotifys: Notifys,
+    dataUser: User
 });
 
 const Store = redux.createStore(allReducers);
