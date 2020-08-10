9954713a04f7fc5de2691fa6a356c38ad32b6f94
LeHieu <Xavan912@gmail.com>
2020-08-10 10:53:56 +0700

    Update UserReducers.js
    + them action dong modal
    + Them action them user va dua ra cac truong hop tra ve cua api

--- src/redux/reducers/UserReducers/UserReducers.js
+++ src/redux/reducers/UserReducers/UserReducers.js
@@ -1,15 +1,34 @@
-const dataUser = {
-    show: false
+const dataNewUser = {
+    show: false,
+    alert: {
+        type    : "info",
+        content : "Hello Word"
+    },
 }
 
 
-const UserReducers = (state = dataUser, action) => {
+const UserReducers = (state = dataNewUser, action) => {
     switch (action.type) {
 
         case 'SHOW_MODAL_ADD_USER':
-            state = { ...state, show: !state.show};
+            state = { ...state, show: true};
+            return state;
+        
+        case 'CLOSE_MODAL_ADD_NEW_USER':
+            state = { ...state, show: false};
             return state;
 
+        case 'ADD_NEW_USER':
+            const dataUser = action.dataForm;
+            /** api them moi user. chu ys co validate du lieu nen ket qua co the khac nhau */
+
+            /** end */
+
+            // Gia su la thanh cong
+            // state = { ...state, show: false,  alert: {...state.alert, type: 'success', content: `Tạo tài khoản cho người dùng ${dataUser.userName} thành công`} };
+            // Gia su la that bai
+            state = { ...state,  alert: {...state.alert, type: 'error', content: `Lỗi email đăng ký không tồn tại hoặc đã đăng ký!`} };
+            return state;
         
         default:
             return state
