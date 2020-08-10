703a5c14e00bb7ea7f8d82253fdf5060033a3d5e
LeHieu <Xavan912@gmail.com>
2020-08-10 15:02:45 +0700

    Update ConfirmDialog.js
    Sua lại giao dien cho cool

--- src/alerts/ConfirmDialog/ConfirmDialog.js
+++ src/alerts/ConfirmDialog/ConfirmDialog.js
@@ -1,28 +1,64 @@
 import React, { useLayoutEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import {
   Button,
   Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  Typography 
+  Typography, 
+  Grid
 } from '@material-ui/core';
-import DeleteIcon from '@material-ui/icons/Delete';
-
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
-  buttonDelete: {
-    color: "#dc004e",
-    border: '1px solid #dc004e80'
-  },
-  buttonClose: {
-    color: "#00000042"
-  }
+    margin: {
+      margin: theme.spacing(1),
+    },
+    content: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    contentBody: {
+        flexGrow: 1,
+        display: 'flex',
+        alignItems: 'center',
+        [theme.breakpoints.down('md')]: {
+          justifyContent: 'center'
+        }
+    },
+    form: {
+        padding: theme.spacing(5),
+        flexBasis: 700,
+        [theme.breakpoints.down('sm')]: {
+          paddingLeft: theme.spacing(2),
+          paddingRight: theme.spacing(2)
+        }
+    },
+    textField: {
+        marginTop: theme.spacing(2)
+    },
+    groupButton: {
+        marginTop: theme.spacing(3)
+    }
 }));
+
+const themeButtonDelete = createMuiTheme({
+  palette: {
+    primary : {
+      main: '#f44336',
+      contrastText: '#fff',
+    },
+  },
+});
+
+const themeButtonClose = createMuiTheme({
+  palette: {
+    primary : {
+      main: '#d0d0d0'
+    },
+  },
+});
 
 const ConfirmDialog = props => {
 
@@ -62,38 +98,47 @@ const ConfirmDialog = props => {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">
-          <Typography variant="h4">
-            { content.title }
-          </Typography>
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            { content.note }
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-
-        <Button 
-          className={ classes.buttonDelete } 
-          variant="outlined" 
-          size="small" 
-          startIcon={<DeleteIcon />}
-          onClick={ handleConfirm }
-        >
-          Xóa
-        </Button>
-
-        <Button 
-          className={ classes.buttonClose } 
-          variant="text" 
-          size="small" 
-          onClick={ handleClose }
-        >
-          Đóng
-        </Button>
-
-        </DialogActions>
+        <Grid
+                className={classes.content}
+            >
+                <div className={classes.content}>
+                    <div className={classes.contentBody}>
+                        <form
+                            className={classes.form}
+                        >
+                            <Typography
+                                gutterBottom
+                                variant="h2"
+                            >
+                            { content.title }
+                            </Typography>
+                            <Typography
+                            color="textSecondary"
+                            gutterBottom
+                            >
+                            { content.note }
+                            </Typography>
+                            <div className={classes.groupButton}>
+                                <ThemeProvider theme={themeButtonDelete}>
+                                    <Button 
+                                        variant="contained" 
+                                        color="primary" 
+                                        onClick={ handleClose }
+                                        startIcon={<DeleteOutlineIcon />}
+                                    >
+                                        Xóa
+                                    </Button>
+                                </ThemeProvider>
+                                <ThemeProvider theme={themeButtonClose}>
+                                    <Button color="primary" className={classes.margin}  onClick={ handleConfirm } >
+                                        Đóng lại
+                                    </Button>
+                                </ThemeProvider>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </Grid>
       </Dialog>
     </div>
   );
