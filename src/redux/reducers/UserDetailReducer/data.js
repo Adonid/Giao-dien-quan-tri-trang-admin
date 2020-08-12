0befa15b3613a389919fd56e11b50e5e628c5af2
LeHieu <Xavan912@gmail.com>
2020-08-12 10:34:32 +0700

    Create data.js
    Them du lieu demo

---
+++ src/redux/reducers/UserDetailReducer/data.js
@@ -0,0 +1,54 @@
+export default {
+  userinfo : 
+  {
+    id: '1222-4334-3234-6765',
+    name: 'Ekaterina Tankova',
+    address: {
+      province: 'TP. Ho Chi Minh',
+      district: 'Tan Binh',
+      commune: 'Phuong 2',
+      street: '18 Dinh Tien Hoang'
+    },
+    email: {
+      verify: false,
+      address: 'ekaterina.tankova@devias.io'
+    },
+    phone: '038-428-3097',
+    avatarUrl: '/images/avatars/avatar_3.png',
+    createdAt: 1555014400000
+  },
+  postinfo :
+  {
+    postUser: 12,
+    postNoApproved: 3,
+    postTotal: 48,
+    topicPosted: 6,
+    following: 76,
+    rateAvg: 4.8,
+    replyComment: 39,
+    allNotifyComment: 107
+  },
+  listSent:
+  [
+    {
+      dateTime: 1555034400000,
+      reason: 'Gửi link reset password'
+    },
+    {
+      dateTime: 1555064400000,
+      reason: 'Gửi thong bao'
+    },
+    {
+      dateTime: 1555234400000,
+      reason: 'Gui thong bao'
+    },
+    {
+      dateTime: 1555456400000,
+      reason: 'Gửi email nhac nho'
+    },
+    {
+      dateTime: 1555123400000,
+      reason: 'Gửi canh bao dong tai khoan'
+    }
+  ]
+};
