8c92b3bf296f90411de32abb77cb1183d1026c57
LeHieu <Xavan912@gmail.com>
2020-08-12 15:07:11 +0700

    Update data.js

--- src/redux/reducers/UserDetailReducer/data.js
+++ src/redux/reducers/UserDetailReducer/data.js
@@ -16,7 +16,7 @@ export default {
     phone: '038-428-3097',
     avatarUrl: '/images/avatars/avatar_3.png',
     createdAt: 1555014400000,
-    isClosed: true
+    isClosed: false
   },
   postinfo :
   {
