b30495391c7ab4f0e0d946be32efc2c082d4b2ca
LeHieu <Xavan912@gmail.com>
2020-08-12 22:19:55 +0700

    Update UserEditor.js
    Tiep tuc hoan thien

--- src/views/UserEditor/UserEditor.js
+++ src/views/UserEditor/UserEditor.js
@@ -1,173 +1,230 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import {
     Link as RouterLink
   } from "react-router-dom";
-  import moment from 'moment';
-  import { makeStyles, ThemeProvider } from '@material-ui/styles';
-  import { 
-      Breadcrumbs, 
-      Link, 
-      Typography, 
-      Button,
-      Grid,
-      Card,
-      CardHeader,
-      Divider,
-      Table,
-      TableBody,
-      TableRow,
-      TableCell,
-      Box,
-      CardContent,
-      createMuiTheme,
-      TextField
-  } from '@material-ui/core';
-  import PerfectScrollbar from 'react-perfect-scrollbar';
-  import EditAttributesOutlinedIcon from '@material-ui/icons/EditAttributesOutlined';
-  import NavigateNextIcon from '@material-ui/icons/NavigateNext';
-  import NotInterestedIcon from '@material-ui/icons/NotInterested';
-  import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
-  import MailOutlineIcon from '@material-ui/icons/MailOutline';
-  import LockOpenIcon from '@material-ui/icons/LockOpen';
-  import { SelectInput } from 'components';
-  import { ConfirmDialog } from 'alerts';
-  
-  const useStyles = makeStyles(theme => ({
-    root: {
-      padding: theme.spacing(3)
-    },
-    content: {
-      marginTop: theme.spacing(5)
-    },
-    row: {
-      height: '42px',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      marginBottom: theme.spacing(0.5)
-      },
-    groupButton: {
-      marginBottom: theme.spacing(2),
-      marginTop: theme.spacing(2),
-      '& button': {
-        marginRight: theme.spacing(1),
-      }
-    },
-    searchInput: {
-      marginRight: theme.spacing(1)
-    },
-    textHighLightVerify: {
-      color: '#4caf50',
-      backgroundColor: '#4caf5014',
-      fontWeight: 500,
-      fontSize: 11,
-      padding: '4px 8px',
-      borderRadius: 3,
-    },
-    textHighLightNoVerify:{
-      color: '#ffab40',
-      backgroundColor: '#ffefc2',
-      fontWeight: 500,
-      fontSize: 11,
-      padding: '4px 8px',
-      borderRadius: 3,
-    },
-    textHighLightInfo: {
-      color: '#29b6f6',
-      backgroundColor: '#e6f8fb',
-      fontWeight: 500,
-      fontSize: 11,
-      padding: '4px 8px',
-      borderRadius: 3,
-    },
-    buttonDelete: {
-      color: '#fff',
-      backgroundColor: '#f44336',
-    },
-    textWarning: {
-      paddingBottom: theme.spacing(1),
-      '& >p' : {
-        color: '#546e7ad9'
-      }
-    },
-    contentSend: {
-      '& .MuiBox-root': {
-        marginTop: theme.spacing(2),
-        marginBottom: theme.spacing(2)
-      }
-    },
-    maxHeightPerfectScrollbar: {
-      maxHeight: 123
-    },
-    isHidenField: {
-      display: 'none'
+import { makeStyles, ThemeProvider } from '@material-ui/styles';
+import { 
+    Breadcrumbs, 
+    Link, 
+    Typography, 
+    Button,
+    Grid,
+    Card,
+    CardHeader,
+    Divider,
+    Table,
+    TableBody,
+    TableRow,
+    TableCell,
+    Box,
+    CardContent,
+    createMuiTheme,
+    TextField,
+    Switch
+} from '@material-ui/core';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3)
+  },
+  content: {
+    marginTop: theme.spacing(5)
+  },
+  customspace: {
+    '& .MuiGrid-root': {
+        '& .MuiTextField-root': {
+            marginTop: theme.spacing(1.5),
+            marginBottom: theme.spacing(1.5),
+        }
     }
-  }));
+  }
+}));
+
+const themeButtonUpdate = createMuiTheme({
+    palette: {
+        primary : {
+        main: '#5850EC',
+        contrastText: '#fff',
+        },
+    },
+});
 
 const UserEditor = props => {
-    return (
-        <div className={ classes.root}>
-            <div>
-                <div className={classes.row}>
-                    <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb" gutterBottom>
-                        <Link color="inherit" component={RouterLink} to="/dashboard">
-                            Dashboard
-                        </Link>
-                        <Link color="inherit" component={RouterLink} to="/users">
-                        Quản lý người dùng
-                        </Link>
-                        <Typography color="textPrimary">{ props.userinfo.name }</Typography>
-                    </Breadcrumbs>
-                    <Button
-                    color="primary"
-                    variant="contained"
-                    // onClick={ showModalAddUser }
-                    >
-                        <EditAttributesOutlinedIcon/> Edit User
-                    </Button>
-                </div>
-                <div>
-                    <Typography variant="h3" gutterBottom>
-                    { props.userinfo.name }
-                    </Typography>
-                </div>
-            </div>
 
-            <div className={classes.content}>
-                <Box>
-                    <Grid container maxWidthLg spacing={5}>
-                        <form>
-                            <Card>
-                                <CardContent>
-                                    <Grid container spacing={3}>
-                                        <Grid
-                                            xs={12} 
-                                            sm={6}
-                                        >
-                                            <TextField id="name-user" label="Tên người dùng *" variant="outlined" />
-                                            <TextField id="email-user" label="Email *" variant="outlined" />
-                                            <TextField id="phone-user" label="Số điện thoại *" variant="outlined" />
-                                        </Grid>
-                                        <Grid
-                                            xs={12} 
-                                            sm={6}
-                                        >
+  const classes = useStyles();
 
-                                        </Grid>
-                                    </Grid>
-                                </CardContent>
-                            </Card>
-                        </form>
-                    </Grid>
-                </Box>
-            </div>
+  const [state, setState] = useState({
+    checkEmailVerify: false,
+    checkOption: false,
+  });
+
+  const handleChangeSwitch = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  return (
+    <div className={classes.root}>
+      <div>
+        <div className={classes.row}>
+            <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb" gutterBottom>
+                <Link color="inherit" component={RouterLink} to="/dashboard">
+                    Dashboard
+                </Link>
+                <Link color="inherit" component={RouterLink} to="/users">
+                  Quản lý người dùng
+                </Link>
+                <Typography color="textPrimary">Ng. Van A</Typography>
+            </Breadcrumbs>
         </div>
-    );
+        <div>
+            <Typography variant="h3" gutterBottom>
+                Ng. Van A
+            </Typography>
+        </div>
+      </div>
+      <div className={classes.content}>
+        <Box>
+            <Card>
+                <CardContent>
+                    <form>
+                        <Grid container spacing={5} className={ classes.customspace}>
+                            <Grid 
+                                item 
+                                xs={12} 
+                                sm={6}
+                                spacing={3}
+                            >
+                                <TextField
+                                    required 
+                                    fullWidth
+                                    label="Họ tên"
+                                    name="userName"
+                                    type="text"
+                                    variant="outlined"
+                                />
+                                <TextField
+                                    required 
+                                    fullWidth
+                                    label="Email"
+                                    name="email"
+                                    type="email"
+                                    variant="outlined"
+                                />
+                                <TextField
+                                    required 
+                                    fullWidth
+                                    label="Số điện thoại"
+                                    name="phone"
+                                    type="text"
+                                    variant="outlined"
+                                />
+                            </Grid>
+                            <Grid 
+                                item 
+                                xs={12} 
+                                sm={6}
+                                spacing={3}
+                            >
+                                <TextField
+                                    fullWidth
+                                    label="Tỉnh/thành phố"
+                                    name="province"
+                                    type="text"
+                                    variant="outlined"
+                                />
+                                <TextField
+                                    fullWidth
+                                    label="Quận/huyện"
+                                    name="district"
+                                    type="text"
+                                    variant="outlined"
+                                />
+                                <TextField
+                                    fullWidth
+                                    label="Phường/xã"
+                                    name="commune"
+                                    type="text"
+                                    variant="outlined"
+                                />
+                                <TextField
+                                    fullWidth
+                                    label="Số nhà/đường/thôn/xóm"
+                                    name="street"
+                                    type="text"
+                                    variant="outlined"
+                                />
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={5} className={ classes.customspace}>
+                            <Grid 
+                                item 
+                                xs={12} 
+                                sm={6}
+                                spacing={3}
+                            >
+                                <Typography variant="h5" color="inherit">
+                                    Xác nhận email
+                                </Typography>
+                                <Typography variant="body1" color="textSecondary">
+                                    Nếu tắt hệ thống sẽ tự gửi email yêu cầu người dùng xác thực lại
+                                </Typography>
+                                <ThemeProvider theme={themeButtonUpdate}>
+                                    <Switch
+                                        checked={state.spacingcheckEmailVerify}
+                                        onChange={handleChangeSwitch}
+                                        color="primary"
+                                        name="checkEmailVerify"
+                                        inputProps={{ 'aria-label': 'primary checkbox' }}
+                                    />
+                                </ThemeProvider>
+                            </Grid>
+                            <Grid 
+                                item 
+                                xs={12} 
+                                sm={6}
+                                spacing={3}
+                            >
+                                <Typography variant="h5" color="inherit">
+                                    Discounted Prices
+                                </Typography>
+                                <Typography variant="body1" color="textSecondary">
+                                    This will give the user discounted prices for all products
+                                </Typography>
+                                <ThemeProvider theme={themeButtonUpdate}>
+                                    <Switch
+                                        checked={state.checkOption}
+                                        onChange={handleChangeSwitch}
+                                        color="primary"
+                                        name="checkOption"
+                                        inputProps={{ 'aria-label': 'primary checkbox' }}
+                                    />
+                                </ThemeProvider>
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={5} className={ classes.customspace}>
+                            <Grid
+                                item 
+                                xs={12} 
+                                sm={6}
+                                spacing={3}
+                            >
+                                
+                                <ThemeProvider theme={themeButtonUpdate}>
+                                    <Button variant="contained" color="primary" disableElevation type="submit">
+                                        CẬP NHẬT USER
+                                    </Button>
+                                </ThemeProvider>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </CardContent>
+            </Card>
+        </Box>
+      </div>
+    </div>
+  );
 };
 
-UserEditor.propTypes = {
-    
-};
 
 export default UserEditor;
