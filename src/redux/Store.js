05bf615d08f610606da7b4aa483ddd3a70204c89
LeHieu <Xavan912@gmail.com>
2020-08-12 10:36:47 +0700

    Update Store.js
    Them reducer

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,13 +1,15 @@
 import {
     NotifysReducers as Notifys,
-    UsersListReducers as UsersList
+    UsersListReducers as UsersList,
+    UserDetailReducer as UsersDetail
 } from './reducers';
 
 const redux = require('redux');
 
 var allReducers = redux.combineReducers({
     dataNotifys: Notifys,
-    dataNewUser: UsersList
+    dataNewUser: UsersList,
+    dataUserDetail: UsersDetail
 });
 
 const Store = redux.createStore(allReducers);
