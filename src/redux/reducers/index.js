d1b9441f8d2361557978c7b8228b5587dcb3e23e
LeHieu <Xavan912@gmail.com>
2020-08-12 10:36:05 +0700

    Update index.js
    Them reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,2 +1,3 @@
 export { default as NotifysReducers  } from './NotifysReducers';
 export { default as UsersListReducers  } from './UsersListReducers';
+export { default as UserDetailReducer  } from './UserDetailReducer';
