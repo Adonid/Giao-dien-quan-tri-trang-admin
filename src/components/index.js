c86e0928e0301912b21da9cfe88fb058aa267678
LeHieu <Xavan912@gmail.com>
2020-08-13 09:35:29 +0700

    Update index.js
    Them component upload va crop anh

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as StatusBullet } from './StatusBullet';
 export { default as RouteWithLayout } from './RouteWithLayout';
 export { default as SelectInput } from './SelectInput';
 export { default as FormAddUser } from './FormAddUser';
+export { default as UploadCropSingleImage } from './UploadCropSingleImage';
