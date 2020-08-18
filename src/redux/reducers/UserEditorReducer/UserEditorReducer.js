7a011b94c295570661f55e42df5b7fa9b893ca1f
LeHieu <Xavan912@gmail.com>
2020-08-18 22:53:09 +0700

    Update UserEditorReducer.js
    Them thong bao ket qua sua user

--- src/redux/reducers/UserEditorReducer/UserEditorReducer.js
+++ src/redux/reducers/UserEditorReducer/UserEditorReducer.js
@@ -2,12 +2,22 @@ import mockData from './data';
 
 const dataUserEditor = {
     dataUser: mockData,
-    alert: {}
+    alert: {
+        type: "info",
+        content: "Hello you"
+    }
 }
 
 const UserEditorReducer = (state = dataUserEditor, action) => {
     switch (action.type) {
-        case '':
+        case 'UPDATE_USER':
+            const dataNewUser = action.data;
+            /** api cap nhat thong tin user */
+                // dataNewUser.img != null ==> cho phep cap nhat avatar
+                // dataNewUser.emailVerify === false ==> email chua xac nhan va gui email yeu cau xac thuc lai
+            /** end */
+            // sau khi api xong dua ra thong bao. vd:
+            state = { ...state, alert: {...state.alert, type: "success", content: "Cập nhật thông tin thành công!"}}
             return state
         
         default:
