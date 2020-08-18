ba25a7f4917b09c90a569fe7374251858ca9bc14
LeHieu <Xavan912@gmail.com>
2020-08-18 22:08:19 +0700

    Update index.js
    Them reducer sua nguoi dung

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,3 +1,4 @@
 export { default as NotifysReducers  } from './NotifysReducers';
 export { default as UsersListReducers  } from './UsersListReducers';
 export { default as UserDetailReducer  } from './UserDetailReducer';
+export { default as UserEditorReducer  } from './UserEditorReducer';
