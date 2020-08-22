651d1b68adf36c827ea0514920ae94fef0860d25
LeHieu <Xavan912@gmail.com>
2020-08-22 15:19:59 +0700

    Update Password.js
    Tien hanh validate va lay du lieu ve. ss de goi store

--- src/views/Settings/components/Password/Password.js
+++ src/views/Settings/components/Password/Password.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
+import validate from 'validate.js';
 import { makeStyles } from '@material-ui/styles';
 import {
   Card,
@@ -16,53 +17,114 @@ const useStyles = makeStyles(() => ({
   root: {}
 }));
 
+const schema = {
+  password: {
+    presence: { allowEmpty: false, message: '^Mật khẩu không để trống' },
+    length: {
+      maximum: 128,
+      message: "^Mật khẩu tối đa 128 ký tự",
+      minimum: 6,
+      message: "^Mật khẩu tối thiểu 6 ký tự",
+    },
+    format: {
+      pattern: "[a-zA-Z0-9]+",
+      flags: "i",
+      message: "^Chỉ bao gồm các ký tự số 0-9, chữ thường và hoa a-zA-Z"
+    }
+  },
+  confirm: {
+    equality: {
+      attribute: "password",
+      message: "^Mật khẩu không khớp!",
+    },
+  },
+};
+
 const Password = props => {
   const { className, ...rest } = props;
 
   const classes = useStyles();
 
-  const [values, setValues] = useState({
-    password: '',
-    confirm: ''
+  const [formState, setFormState] = useState({
+    isValid: false,
+    values: {},
+    touched: {},
+    errors: {}
   });
 
+  useEffect(() => {
+    const errors = validate(formState.values, schema);
+
+    setFormState(formState => ({
+      ...formState,
+      isValid: errors ? false : true,
+      errors: errors || {}
+    }));
+  }, [formState.values]);
+
   const handleChange = event => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value
-    });
+    event.persist();
+
+    setFormState(formState => ({
+      ...formState,
+      values: {
+        ...formState.values,
+        [event.target.name]:
+          event.target.type === 'checkbox'
+            ? event.target.checked
+            : event.target.value
+      },
+      touched: {
+        ...formState.touched,
+        [event.target.name]: true
+      }
+    }));
   };
+
+  const hasError = field => formState.touched[field] && formState.errors[field] ? true : false;
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(formState.values.password);
+    // call api
+  }
 
   return (
     <Card
       {...rest}
       className={clsx(classes.root, className)}
     >
-      <form>
+      <form onSubmit={ handleSubmit }>
         <CardHeader
-          subheader="Update password"
-          title="Password"
+          subheader="Cập nhật mật khẩu"
+          title="Mật khẩu"
         />
         <Divider />
         <CardContent>
           <TextField
             fullWidth
-            label="Password"
+            label="Mật khẩu"
             name="password"
-            onChange={handleChange}
             type="password"
-            value={values.password}
             variant="outlined"
+            error={hasError('password')}
+            helperText={
+              hasError('password') ? formState.errors.password[0] : null
+            }
+            onChange={handleChange}
           />
           <TextField
             fullWidth
-            label="Confirm password"
+            label="Xác nhận mật khẩu"
             name="confirm"
-            onChange={handleChange}
             style={{ marginTop: '1rem' }}
             type="password"
-            value={values.confirm}
             variant="outlined"
+            error={hasError('confirm')}
+            helperText={
+              hasError('confirm') ? formState.errors.confirm[0] : null
+            }
+            onChange={handleChange}
           />
         </CardContent>
         <Divider />
@@ -70,6 +132,8 @@ const Password = props => {
           <Button
             color="primary"
             variant="outlined"
+            type="submit"
+            disabled={!formState.isValid}
           >
             Update
           </Button>
