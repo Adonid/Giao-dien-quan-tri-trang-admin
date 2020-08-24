37cf916599acc4c9fa8e1ac90628abae8c991cfb
LeHieu <Xavan912@gmail.com>
2020-08-24 10:56:45 +0700

    Update Chats.js
    Bo cuc lai component chats

--- src/views/Chats/Chats.js
+++ src/views/Chats/Chats.js
@@ -1,34 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
-import { 
-    Grid 
-} from '@material-ui/core';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/styles';
+import { useMediaQuery } from '@material-ui/core';
+import { ChatsList } from './components';
+
 
 const useStyles = makeStyles(theme => ({
     root: {
-      display: "flex",
-      flexGrow: 1,
-    }
+        paddingTop: 35,
+        height: '100%',
+        [theme.breakpoints.up('md')]: {
+          paddingTop: 45
+        }
+    },
+    shiftContent: {
+        paddingLeft: 256
+      },
+    content: {
+        height: '100%'
+      }
   }));
 
 const Chats = props => {
+    const { children } = props;
 
     const classes = useStyles();
+    const theme = useTheme();
+    const isDesktop = useMediaQuery(theme.breakpoints.up('md'), {
+      defaultMatches: true
+    });
+  
+    const [openSidebar, setOpenSidebar] = useState(false);
+  
+    const [openAddUser, setOpenAddUser] = useState(false);
+  
+    const handleSidebarOpen = () => {
+      setOpenSidebar(true);
+    };
+  
+    const handleChatsListClose = () => {
+      setOpenSidebar(false);
+    };
+  
+    const shouldOpenChatsList = isDesktop ? true : openSidebar;
 
     return (
-        <div className={ classes.root }>
-            <Grid container className={classes.root} spacing={2}>
-                <Grid item>
-
-                </Grid>
-            </Grid>
+        <div 
+        className={clsx({
+            [classes.root]: true,
+            [classes.shiftContent]: isDesktop
+          })}
+        >
+            <ChatsList
+                onClose={handleChatsListClose}
+                open={shouldOpenChatsList}
+                variant={isDesktop ? 'persistent' : 'temporary'}
+            />
+            <main className={classes.content}>
+                {children}
+            </main>
         </div>
     );
 };
 
 Chats.propTypes = {
-    
+    children: PropTypes.node
 };
 
 export default Chats;
