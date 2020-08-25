c09942481633727b077ade2ee09d1a10eaa2dbdc
LeHieu <Xavan912@gmail.com>
2020-08-25 13:50:28 +0700

    Update Chats.js
    Thay doi lai layout

--- src/views/Chats/Chats.js
+++ src/views/Chats/Chats.js
@@ -2,65 +2,50 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/styles';
-import { useMediaQuery } from '@material-ui/core';
-import { ChatsList } from './components';
+import { 
+  Grid, 
+  Box,
+  Hidden,
+  Paper
+
+} from '@material-ui/core';
+import { ChatsList, ConversationsChat } from './components';
 
 
 const useStyles = makeStyles(theme => ({
     root: {
-        paddingTop: 35,
-        height: '100%',
-        [theme.breakpoints.up('md')]: {
-          paddingTop: 45
-        }
+      flexGrow: 1,
     },
-    shiftContent: {
-        paddingLeft: 256
-      },
-    content: {
-        height: '100%'
-      }
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    heightChatList: {
+      height: "100%"
+    }
   }));
 
 const Chats = props => {
     const { children } = props;
 
     const classes = useStyles();
-    const theme = useTheme();
-    const isDesktop = useMediaQuery(theme.breakpoints.up('md'), {
-      defaultMatches: true
-    });
-  
-    const [openSidebar, setOpenSidebar] = useState(false);
-  
-    const [openAddUser, setOpenAddUser] = useState(false);
-  
-    const handleSidebarOpen = () => {
-      setOpenSidebar(true);
-    };
-  
-    const handleChatsListClose = () => {
-      setOpenSidebar(false);
-    };
-  
-    const shouldOpenChatsList = isDesktop ? true : openSidebar;
 
     return (
-        <div 
-        className={clsx({
-            [classes.root]: true,
-            [classes.shiftContent]: isDesktop
-          })}
-        >
-            <ChatsList
-                onClose={handleChatsListClose}
-                open={shouldOpenChatsList}
-                variant={isDesktop ? 'persistent' : 'temporary'}
-            />
-            <main className={classes.content}>
-                {children}
-            </main>
-        </div>
+        <Box >
+          <Grid container>
+              <Hidden xsDown>
+                <ChatsList classes={ classes.heightChatList }/>
+              </Hidden>
+              <Hidden smUp>
+                <Paper className={classes.paper}>NOI DUNG DANH CHO MAN HINH IPAD MINI TRO XUONG </Paper>
+              </Hidden>
+            <Grid item xs={8}>
+              <main>
+                <ConversationsChat/>
+              </main>
+            </Grid>
+          </Grid>
+        </Box>
     );
 };
 
