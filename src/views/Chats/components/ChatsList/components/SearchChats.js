b625a2251d971c61e653da92a3dd8f6d46c9040d
LeHieu <Xavan912@gmail.com>
2020-08-27 10:06:46 +0700

    Create SearchChats.js
    Tach tu chatslist ra components  SearchChats

---
+++ src/views/Chats/components/ChatsList/components/SearchChats.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import {  
+    InputBase,
+    Box,
+ } from '@material-ui/core';
+ import SearchIcon from '@material-ui/icons/Search';
+
+ const useStyles = makeStyles(theme => ({
+    root: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      marginBottom: theme.spacing(2),
+    },
+    search: {
+      height: theme.spacing(5.5),
+      display: 'flex',
+      alignItems: 'center',
+      paddingLeft: theme.spacing(2),
+      borderRadius: theme.spacing(2.8),
+      paddingRight: theme.spacing(2),
+      backgroundColor: '#f4f6f8',
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: '#607d8bd6'
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '25ch',
+        '&:focus': {
+          width: '33ch',
+        },
+      },
+      [theme.breakpoints.down('md')]: {
+        width: '15ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    }
+    }));
+
+const SearchChats = props => {
+
+    const classes = useStyles();
+
+    return (
+        <Box className={ classes.root}>
+            <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                <SearchIcon />
+                </div>
+                <InputBase
+                placeholder="Search…"
+                classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+                />
+            </div>
+        </Box>
+    );
+};
+
+SearchChats.propTypes = {
+    
+};
+
+export default SearchChats;
