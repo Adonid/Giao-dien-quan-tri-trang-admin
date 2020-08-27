b1ebf217188cb8f5b62f46fd765a54f16f6af321
LeHieu <Xavan912@gmail.com>
2020-08-27 10:06:59 +0700

    Create TopHeaderChat.js
    Tach tu chatslist ra components TopHeaderChat

---
+++ src/views/Chats/components/ChatsList/components/TopHeaderChat.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createMuiTheme, ThemeProvider, fade } from '@material-ui/core/styles';
+import {  
+    CardHeader,
+    Typography,
+    IconButton,
+    Badge,
+
+ } from '@material-ui/core';
+ import AccessTimeOutlinedIcon from '@material-ui/icons/AccessTimeOutlined';
+ import ChatBubbleOutlineOutlinedIcon from '@material-ui/icons/ChatBubbleOutlineOutlined';
+ import PeopleAltOutlinedIcon from '@material-ui/icons/PeopleAltOutlined';
+
+ const themeBadgeMessage = createMuiTheme({
+    palette: {
+      error : {
+        main: '#8bc34a',
+        contrastText: '#fff',
+      },
+    },
+  });
+
+const TopHeaderChat = props => {
+
+    return (
+        <CardHeader
+            action={
+                <React.Fragment>
+                <IconButton aria-label="settings">
+                    <Badge
+                    variant="dot"
+                    color="secondary"
+                    >
+                    <AccessTimeOutlinedIcon />
+                    </Badge>
+                </IconButton>
+                <IconButton aria-label="chat">
+                    <ThemeProvider theme={themeBadgeMessage}>
+                    <Badge
+                        badgeContent={2}
+                        color="error"
+                    >
+                        <ChatBubbleOutlineOutlinedIcon />
+                    </Badge>
+                    </ThemeProvider>
+                </IconButton>
+                <IconButton aria-label="group">
+                    <ThemeProvider theme={themeBadgeMessage}>
+                    <Badge
+                        variant="dot"
+                        color="error"
+                    >
+                        <PeopleAltOutlinedIcon />
+                    </Badge>
+                    </ThemeProvider>
+                </IconButton>
+                </React.Fragment>
+            }
+            title={<Typography gutterBottom variant="h3" component="h2">Chats</Typography>}
+        />
+    );
+};
+
+TopHeaderChat.propTypes = {
+    
+};
+
+export default TopHeaderChat;
