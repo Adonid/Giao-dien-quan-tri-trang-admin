96c80c742ed748192bfc9bb0b4ee455dda2dd1b0
LeHieu <Xavan912@gmail.com>
2020-08-31 10:53:14 +0700

    Create dataSingle.js
    demo data don

---
+++ src/redux/reducers/ChatsDataReducer/dataSingle.js
@@ -0,0 +1,30 @@
+
+export default [
+    { 
+        id: 54,
+        isSelected: false, 
+        avatar: '/images/avatars/avatar_7.png', 
+        name: "Hải Đăng", 
+        content: "Bạn vui lòng inbox mình nha!", 
+        time: "15 phút trước", 
+        isReaded: true
+    },
+    { 
+        id: 43,
+        isSelected: false, 
+        avatar: '/images/avatars/avatar_8.png', 
+        name: "Phan Trang", 
+        content: "Ship hàng trong bao nhiêu lâu ạ?", 
+        time: "30 phút trước", 
+        isReaded: false
+    },
+    { 
+        id: 23,
+        isSelected: false, 
+        avatar: '/images/avatars/avatar_9.png', 
+        name: "Chí Quốc", 
+        content: "Ok bạn O))", 
+        time: "1 giờ trước", 
+        isReaded: true
+    },
+];
