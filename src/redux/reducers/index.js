399df2cd4a8fb8ea58e1892c454d84d80cc08c31
LeHieu <Xavan912@gmail.com>
2020-08-31 09:51:58 +0700

    Update index.js
    Them reducer Chats

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,3 +2,4 @@ export { default as NotifysReducers  } from './NotifysReducers';
 export { default as UsersListReducers  } from './UsersListReducers';
 export { default as UserDetailReducer  } from './UserDetailReducer';
 export { default as UserEditorReducer  } from './UserEditorReducer';
+export { default as ChatsDataReducer  } from './ChatsDataReducer';
