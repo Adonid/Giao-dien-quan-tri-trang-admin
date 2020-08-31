d2853d1585240ad593cd7bf4a6263a58a511758e
LeHieu <Xavan912@gmail.com>
2020-08-31 11:01:03 +0700

    Update dataGroup.js

--- src/redux/reducers/ChatsDataReducer/dataGroup.js
+++ src/redux/reducers/ChatsDataReducer/dataGroup.js
@@ -27,4 +27,31 @@ export default [
         time: "1 giờ trước", 
         isReaded: true
     },
+    { 
+        id: 87,
+        isSelected: false, 
+        avatar: '/images/avatars/avatar_4.png', 
+        name: "Vân Dung", 
+        content: "Bạn vui lòng inbox mình nha!", 
+        time: "15 phút trước", 
+        isReaded: true
+    },
+    { 
+        id: 14,
+        isSelected: false, 
+        avatar: '/images/avatars/avatar_5.png', 
+        name: "Nguyễn Trọng Hoàng", 
+        content: "Ship hàng trong bao nhiêu lâu ạ?", 
+        time: "30 phút trước", 
+        isReaded: false
+    },
+    { 
+        id: 78,
+        isSelected: false, 
+        avatar: '/images/avatars/avatar_6.png', 
+        name: "Trinh Uyên", 
+        content: "Ok bạn O))", 
+        time: "1 giờ trước", 
+        isReaded: true
+    },
 ];
