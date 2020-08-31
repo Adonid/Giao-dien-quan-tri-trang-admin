69cb6ed5fdfc2d3515feca205c71ed5c508fd1a4
LeHieu <Xavan912@gmail.com>
2020-08-31 09:52:06 +0700

    Create data.js
    Them reducer Chats

---
+++ src/redux/reducers/ChatsDataReducer/data.js
@@ -0,0 +1,30 @@
+
+export default [
+    { 
+        id: 1,
+        isSelected: false, 
+        avatar: '/images/avatars/avatar_3.png', 
+        name: "Hoàng Trọng Hải", 
+        content: "Bạn vui lòng inbox mình nha!", 
+        time: "1 phút trước", 
+        isReaded: false
+    },
+    { 
+        id: 2,
+        isSelected: false, 
+        avatar: '/images/avatars/avatar_2.png', 
+        name: "Duy Mạnh", 
+        content: "Ship hàng trong bao nhiêu lâu ạ?", 
+        time: "30 phút trước", 
+        isReaded: false
+    },
+    { 
+        id: 3,
+        isSelected: false, 
+        avatar: '/images/avatars/avatar_1.png', 
+        name: "Văn Trường", 
+        content: "Ok bạn O))", 
+        time: "2 giờ trước", 
+        isReaded: true
+    },
+];
