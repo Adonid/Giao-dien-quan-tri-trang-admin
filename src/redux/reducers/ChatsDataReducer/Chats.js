f8fb97f0ed3f1a56bc1d43cb64a6dbdfee9d9e04
LeHieu <Xavan912@gmail.com>
2020-08-31 10:20:12 +0700

    Update Chats.js
    Set trang thai cho cac item chat

--- src/redux/reducers/ChatsDataReducer/Chats.js
+++ src/redux/reducers/ChatsDataReducer/Chats.js
@@ -9,7 +9,19 @@ const Chats = (state = ChatsData, action) => {
     switch (action.type) {
         case 'VIEW_ITEM_CHAT':
             const id = action.id;
-            console.log(id);
+            /** api danh dau item la da doc & truyen data itemchat qua component cuoi hoi thoai */
+
+            /** end */
+            // Sau do loc du lieu ve trang thai item chat
+            let updateChatsList = [...state.chatsList];
+            updateChatsList.forEach( el => {
+                el.isSelected=false;
+                if(el.id===id){
+                    el.isReaded=true;
+                    el.isSelected=true;
+                }
+            });
+            state = { ...state, chatsList: updateChatsList};
             return state
         
         default:
