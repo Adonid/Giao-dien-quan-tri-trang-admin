a32bea6364550d7b47723a977d4556881a9405cc
LeHieu <Xavan912@gmail.com>
2020-08-31 14:58:08 +0700

    Update ConversationsChat.js
    Xu ly phan giao dien
    Phan header

--- src/views/Chats/components/ConversationsChat/ConversationsChat.js
+++ src/views/Chats/components/ConversationsChat/ConversationsChat.js
@@ -1,25 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { red } from '@material-ui/core/colors';
 import { 
     Paper, 
     Card, 
     CardHeader,
     Typography,
-    Divider
+    Divider,
+    Box,
+    Avatar
  } from '@material-ui/core';
+ import IconButton from '@material-ui/core/IconButton';
+ import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+ const useStyles = makeStyles(theme => ({
+    root: {
+      
+    },
+    avatar: {
+        backgroundColor: red[500],
+        textTransform: 'uppercase'
+      },
+    }));
 
 const ConversationsChat = props => {
     return (
-        <Paper>
-            <Card>
-                <CardHeader>
-                <Typography component="h3" variant="h2">
-                    Live From Space
-                </Typography>
-                </CardHeader>
-                <Divider/>
-            </Card>
-        </Paper>
+        <Box>
+            <Paper variant="outlined" square={true}>
+                <Card>
+                    <CardHeader
+                        avatar={
+                            dataItem.avatar
+                            ?
+                            <Avatar
+                                src={ dataItem.avatar }
+                            />
+                            :
+                            <Avatar
+                                className={classes.avatar}
+                                aria-label="recipe"
+                            >
+                                { dataItem.name.substring(0, 1) }
+                            </Avatar>
+                        }
+                        action={
+                            <IconButton aria-label="settings">
+                                <MoreVertIcon />
+                            </IconButton>
+                        }
+                        title={ <Typography variant="h5">Nguyễn Đăng Dung</Typography>}
+                        subheader={ <Typography variant="caption">Vừa mới truy cập</Typography>}
+                    />
+                    <Divider/>
+                </Card>
+            </Paper>
+        </Box>
     );
 };
 
