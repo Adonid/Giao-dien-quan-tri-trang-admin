4fb607dadb1b7c3a15b3e5f87ff204437fdac1fa
LeHieu <Xavan912@gmail.com>
2020-09-04 08:39:39 +0700

    Update ProductsToolbar.js
    Ke thua tu giao dien cua danh sach nguoi dung

--- src/views/ProductList/components/ProductsToolbar/ProductsToolbar.js
+++ src/views/ProductList/components/ProductsToolbar/ProductsToolbar.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  Link as RouterLink
+} from "react-router-dom";
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/styles';
-import { Button } from '@material-ui/core';
-
-import { SearchInput } from 'components';
-
-import Typography from '@material-ui/core/Typography';
-import Breadcrumbs from '@material-ui/core/Breadcrumbs';
-import Link from '@material-ui/core/Link';
+import { 
+  Button, 
+  Typography,
+  Breadcrumbs,
+  Link
+ } from '@material-ui/core';
+ import PostAddOutlinedIcon from '@material-ui/icons/PostAddOutlined';
+  import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import { connect } from 'react-redux';
 
 const useStyles = makeStyles(theme => ({
   root: {},
@@ -16,80 +21,54 @@ const useStyles = makeStyles(theme => ({
     height: '42px',
     display: 'flex',
     alignItems: 'center',
-    marginTop: theme.spacing(1)
-  },
-  spacer: {
-    flexGrow: 1
-  },
-  importButton: {
-    marginRight: theme.spacing(1)
-  },
-  exportButton: {
-    marginRight: theme.spacing(1)
+    justifyContent: 'space-between',
+    marginBottom: theme.spacing(0.5)
+    },
+  groupButton: {
+    marginBottom: theme.spacing(2),
+    marginTop: theme.spacing(2),
+    '& button': {
+      marginRight: theme.spacing(1),
+    }
   },
   searchInput: {
     marginRight: theme.spacing(1)
-  },
-  flexRow: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  titlePage: {
-    paddingTop: theme.spacing(1.5),
-    paddingBottom: theme.spacing(1.8),
-    textTransform: "uppercase",
-    color: "#546e7a"
   }
 }));
-
-function handleClick(event) {
-  event.preventDefault();
-  console.info('You clicked a breadcrumb.');
-}
 
 const ProductsToolbar = props => {
   const { className, ...rest } = props;
 
   const classes = useStyles();
 
+  const showModalAddUser = () => {
+    props.showModalAddNewUser();
+}
+
   return (
     <div
       {...rest}
       className={clsx(classes.root, className)}
     >
-      <div className={classes.flexRow}>
-        <div>
-          <Breadcrumbs aria-label="breadcrumb">
-            <Link color="inherit" href="/" onClick={handleClick}>
-              Dashboad
-            </Link>
-            <Link color="inherit" href="/getting-started/installation/" onClick={handleClick}>
-              Du lịch
-            </Link>
-            <Typography color="textPrimary">Breadcrumb</Typography>
-          </Breadcrumbs>
-          <Typography variant="h4" className={classes.titlePage}>
-            Đến Hà Thành đừng quên ghé thăm 3 địa điểm du lịch cổ xưa này
-          </Typography>
-        </div>
-        <div>
-          <span className={classes.spacer} />
-          <Button className={classes.importButton}>Import</Button>
-          <Button className={classes.exportButton}>Export</Button>
-          <Button
-            color="primary"
-            variant="contained"
-          >
-            Add product
-          </Button>
-        </div>
-      </div>
       <div className={classes.row}>
-        <SearchInput
-          className={classes.searchInput}
-          placeholder="Search product"
-        />
+        <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb" gutterBottom>
+          <Link color="inherit" component={RouterLink} to="/dashboard">
+              Dashboard
+          </Link>
+          <Typography color="textPrimary">Quản lý bài viết</Typography>
+        </Breadcrumbs>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={ showModalAddUser }
+        >
+          <PostAddOutlinedIcon/> Thêm mới
+        </Button>
+      </div>
+      <div>
+        <Typography variant="h3" gutterBottom>
+          Quản lý bài viết
+        </Typography>
       </div>
     </div>
   );
@@ -99,4 +78,20 @@ ProductsToolbar.propTypes = {
   className: PropTypes.string
 };
 
-export default ProductsToolbar;
+  const mapStateToProps = (state, ownProps) => {
+    return {
+      prop: state.prop
+    }
+  }
+
+  const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+      showModalAddNewUser: () => {
+        dispatch({
+          type: 'SHOW_MODAL_ADD_USER'
+        })
+      }
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductsToolbar)
