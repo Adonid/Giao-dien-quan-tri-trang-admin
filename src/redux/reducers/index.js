83ea50e7b7041a8debc02c636d8d4c03f821f580
LeHieu <Xavan912@gmail.com>
2020-09-04 09:46:35 +0700

    Update index.js
    Them reducer cho danh sach cac bai viet

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,3 +3,4 @@ export { default as UsersListReducers  } from './UsersListReducers';
 export { default as UserDetailReducer  } from './UserDetailReducer';
 export { default as UserEditorReducer  } from './UserEditorReducer';
 export { default as ChatsDataReducer  } from './ChatsDataReducer';
+export { default as ProductsListReducer  } from './ProductsListReducer';
