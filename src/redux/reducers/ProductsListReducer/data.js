d2f024383d5736818accc80ef011ece3cad4ac00
LeHieu <Xavan912@gmail.com>
2020-09-04 11:04:21 +0700

    Update data.js
    Them du lieu demo

--- src/redux/reducers/ProductsListReducer/data.js
+++ src/redux/reducers/ProductsListReducer/data.js
@@ -4,90 +4,140 @@ export default [
   {
     id: uuid(),
     name: '3 loại cây bạn nên đặt trong phòng ngủ',
-    avatar: '/images/avatars/avatar_1.png',
+    author: {
+        avatar: '/images/avatars/avatar_1.png',
+        name: "Bùi Trọng Hoàng"
+    },
     category: 'Sức khỏe',
     rating: 3.9,
+    like: 219,
+    viewer: 1027,
     active: true, 
     createdAt: 1555014400000
   },
   {
     id: uuid(),
     name: 'Những loại cây giúp xua muỗi đuổi chuột',
-    avatar: '/images/avatars/avatar_2.png',
+    author: {
+        avatar: '/images/avatars/avatar_2.png',
+        name: "Vân Dung"
+    },
     category: 'Sức khỏe',
     rating: 3.7,
+    like: 219,
+    viewer: 1027,
     active: true, 
     createdAt: 1555096400000
   },
   {
     id: uuid(),
     name: '5 cách tự nhiên để đuổi muỗi trong nhà',
-    avatar: '/images/avatars/avatar_3.png',
+    author: {
+        avatar: '/images/avatars/avatar_3.png',
+        name: "Thúy Nga"
+    },
     category: 'Sức khỏe',
     rating: 4.1,
+    like: 219,
+    viewer: 1027,
     active: true, 
     createdAt: 1555017400000
   },
   {
     id: uuid(),
     name: 'Cách chăm sóc trẻ bị sốt',
-    avatar: '/images/avatars/avatar_4.png',
+    author: {
+        avatar: '/images/avatars/avatar_4.png',
+        name: "Lê Kim Dũng"
+    },
     category: 'Sức khỏe',
     rating: 3.8,
+    like: 219,
+    viewer: 1027,
     active: true, 
     createdAt: 1554990000000
   },
   {
     id: uuid(),
     name: 'Vì sao cá nóc vừa độc vừa đắt lại hút khách?',
-    avatar: '/images/avatars/avatar_5.png',
+    author: {
+        avatar: '/images/avatars/avatar_5.png',
+        name: "Nguyễn Khai Quang"
+    },
     category: 'Du lịch',
     rating: 3.9,
+    like: 219,
+    viewer: 1027,
     active: true, 
     createdAt: 1554707200000
   },
   {
     id: uuid(),
     name: 'Thắng cảnh trên cao nguyên Di Linh',
-    avatar: '/images/avatars/avatar_6.png',
+    author: {
+        avatar: '/images/avatars/avatar_6.png',
+        name: "Bách Dụ"
+    },
     category: 'Du lịch',
     rating: 3.2,
+    like: 219,
+    viewer: 1027,
     active: true, 
     createdAt: 1554671800000
   },
   {
     id: uuid(),
     name: 'Ghềnh Bàng hoang sơ giữa bán đảo Sơn Trà',
-    avatar: '/images/avatars/avatar_7.png',
+    author: {
+        avatar: '',
+        name: "Kim Trung"
+    },
     category: 'Du lịch',
     rating: 3.9,
+    like: 219,
+    viewer: 1027,
     active: true, 
     createdAt: 1554323200000
   },
   {
     id: uuid(),
     name: 'Phát hiện loài chim có tiếng hót cao nhất',
-    avatar: '/images/avatars/avatar_8.png',
+    author: {
+        avatar: '/images/avatars/avatar_8.png',
+        name: "Cao Kiến Huy"
+    },
     category: 'Khoa học',
     rating: 3.9,
+    like: 219,
+    viewer: 1027,
     active: true, 
     createdAt: 1523048600000
   },
   {
     id: uuid(),
     name: 'Mạch nước phun trào cao hơn 30 mét',
-    avatar: '/images/avatars/avatar_9.png',
+    author: {
+        avatar: '/images/avatars/avatar_9.png',
+        name: "Minh Hoàng"
+    },
     category: 'Khoa học',
     rating: 4.4,
+    like: 219,
+    viewer: 1027,
     active: true,
     createdAt: 1522702400000
   },
   {
     id: uuid(),
     name: 'Cặp cá voi lưng gù thi nhau nhảy lên mặt nước',
-    avatar: '/images/avatars/avatar_10.png',
+    author: {
+        avatar: '',
+        name: "Vi Xuân Dụ"
+    },
     category: 'Khoa học',
     rating: 3.1,
+    like: 219,
+    viewer: 1027,
     active: true,
     createdAt: 1522702100000
   }
