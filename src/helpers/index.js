6b56eed75a93181c4a29b4e1c7b695f9287b6bf9
LeHieu <Xavan912@gmail.com>
2020-09-05 14:03:44 +0700

    Update index.js
    Them vao helper function toSlug

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
 export { default as chartjs } from './chartjs';
 export { default as getInitials } from './getInitials';
 export { default as filterPost } from './filterPost';
+export { default as toSlug } from './toSlug';
