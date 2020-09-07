19553880ba21e455459810a5d07e89e8fd2b26c0
LeHieu <Xavan912@gmail.com>
2020-09-07 08:02:22 +0700

    Create AddNewProduct.js
    Them AddNewProduct

---
+++ src/views/AddNewProduct/AddNewProduct.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const AddNewProduct = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+AddNewProduct.propTypes = {
+    
+};
+
+export default AddNewProduct;
