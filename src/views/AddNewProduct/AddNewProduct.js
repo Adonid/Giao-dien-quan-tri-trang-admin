d30978afa53fefc45cf1ce4ce502e400863b4c0b
LeHieu <Xavan912@gmail.com>
2020-09-07 08:20:24 +0700

    Update AddNewProduct.js
    Dang xu ly phan toolbar

--- src/views/AddNewProduct/AddNewProduct.js
+++ src/views/AddNewProduct/AddNewProduct.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import { ProductsToolbar } from './components';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      padding: theme.spacing(3)
+    },
+    content: {
+      marginTop: theme.spacing(2)
+    }
+  }));
 
 const AddNewProduct = props => {
+    const classes = useStyles();
+
     return (
-        <div>
-            
+        <div className={classes.root}>
+            <ProductsToolbar />
+            <div className={classes.content}>
+                {/* <ProductCard /> */}
+            </div>
         </div>
     );
 };
