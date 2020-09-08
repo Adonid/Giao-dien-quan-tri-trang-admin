9ae690f66277bc56971e4eeeeeba3686b68fd152
LeHieu <Xavan912@gmail.com>
2020-09-08 17:08:51 +0700

    Update TinyMCE.js
    Cau hinh tinymce
    - Tai sao insert/edit image lai ko click vao field duoc?
    - Responsive filemannager da tich hp duoc nhung lai khong hoat dong dung chuc nang

--- src/components/TinyMCE/TinyMCE.js
+++ src/components/TinyMCE/TinyMCE.js
@@ -83,74 +83,32 @@ const TinyMCE = props => {
                                 init={{
                                     height: 500,
                                     menubar: true,
-                                    plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
-                                    toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen preview print | insertfile image media template link anchor codesample | ltr rtl',
-                                    toolbar_sticky: false,
+                                    plugins: [
+                                        'print preview paste importcss searchreplace autolink autosave save',
+                                        'directionality code visualblocks visualchars fullscreen image link media',
+                                        'template codesample table charmap hr pagebreak nonbreaking anchor toc',
+                                        'insertdatetime advlist lists wordcount imagetools textpattern noneditable',
+                                        'help charmap quickbars emoticons responsivefilemanager',
+                                    ],
+                                    toolbar: 'undo redo | bold italic underline strikethrough | ' +
+                                            'fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | ' +
+                                            'outdent indent |  numlist bullist | forecolor backcolor removeformat | ' +
+                                            'pagebreak | charmap emoticons | fullscreen preview print | ' +
+                                            'insertfile image media template link anchor codesample | ltr rtl',
                                     autosave_ask_before_unload: true,
                                     autosave_interval: "30s",
-                                    autosave_prefix: "{path}{query}-{id}-",
-                                    autosave_restore_when_empty: false,
                                     autosave_retention: "2m",
                                     a11y_advanced_options: true,
-                                    image_advtab: true,
                                     image_caption: true,
-                                    image_list: [
-                                        {title: '<img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png" alt="nice" />', value: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png' },
-                                        {title: 'My image 2', value: 'https://i.ytimg.com/an_webp/qCdE6IL62yQ/mqdefault_6s.webp?du=3000&sqp=CNDAi_cF&rs=AOn4CLB1spjuimjOpVyQsCRICCHRObjWRw' }
-                                    ],
-                                    image_class_list: [
-                                        { title: 'None', value: '' },
-                                        { title: 'Some class', value: 'img-fluid rounded' }
-                                    ],
-                                    // file_picker_types: 'image',  // file image media
-                                    automatic_uploads: false,
-                                    file_picker_callback: function (callback, value, meta) {
-                                        /* Provide file and text for the link dialog */
-                                        if (meta.filetype === 'file') {
-                                        callback('https://www.youtube.com/watch?v=JHHYXN52oUo&list=RDJHHYXN52oUo&start_radio=1', { text: 'My text' });
-                                        }
-                                        /* Provide image and alt text for the image dialog */
-                                        if (meta.filetype === 'image') {
-                                            var input = document.createElement('input');
-                                            input.setAttribute('type', 'file');
-                                            input.setAttribute('accept', 'image/*');
-
-                                            /*
-                                            Note: In modern browsers input[type="file"] is functional without
-                                            even adding it to the DOM, but that might not be the case in some older
-                                            or quirky browsers like IE, so you might want to add it to the DOM
-                                            just in case, and visually hide it. And do not forget do remove it
-                                            once you do not need it anymore.
-                                            */
-                                            input.onchange = function () {
-                                            var file = this.files[0];
-                                            var reader = new FileReader();
-                                            reader.addEventListener("load", function () {
-                                                /*
-                                                Note: Now we need to register the blob in TinyMCEs image blob
-                                                registry. In the next release this part hopefully won't be
-                                                necessary, as we are looking to handle it internally.
-                                                */                                   
-                                                const ext = file.name.substring(file.name.lastIndexOf('.') + 1);
-                                                console.log(ext);
-                                                
-                                                const imgbase64 = reader.result.split(',')[1];
-                                                console.log(imgbase64);
-
-                                            /* Call AXIOS -> API HERE */
-                                            
-                                            /* call the callback and populate the Title field with the file name */
-                                                callback('url_image_from_API', { title: file.name });
-                                            });
-                                            reader.readAsDataURL(file);
-                                            };
-                                            input.click();
-                                        }
-                                        /* Provide alternative source and posted for the media dialog */
-                                        if (meta.filetype === 'media') {
-                                        callback('https://www.youtube.com/watch?v=JHHYXN52oUo&list=RDJHHYXN52oUo&start_radio=1', { source2: 'alt.ogg', poster: 'https://www.youtube.com/watch?v=JHHYXN52oUo&list=RDJHHYXN52oUo&start_radio=1' });
-                                        }
+                                    image_advtab: true,
+                                    // file_picker_types: 'image',  // file image only media
+                                    external_filemanager_path: "responsive_filemanager/",
+                                    filemanager_title: "Trình quản lý ảnh upload",
+                                    external_plugins: {
+                                        "responsivefilemanager": "/responsive_filemanager/tinymce/plugins/responsivefilemanager/plugin.min.js",
+                                        "filemanager": "/responsive_filemanager/filemanager/plugin.min.js"
                                     },
+                                    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
                                 }}
                                 onChange={e => setContentPost(e.target.getContent())}
                             />
