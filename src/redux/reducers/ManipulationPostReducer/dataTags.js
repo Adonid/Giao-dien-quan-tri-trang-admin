1f2187102ef5693b766536bdde2eb11c81986468
LeHieu <Xavan912@gmail.com>
2020-09-14 13:56:54 +0700

    Create dataTags.js
    Them du lieu tags

---
+++ src/redux/reducers/ManipulationPostReducer/dataTags.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        id: 1,
+        label: "Sinh nhật"
+    },
+    {
+        id: 2,
+        label: "Trung Thu"
+    },
+    {
+        id: 3,
+        label: "Tết cổ truyền"
+    },
+    {
+        id: 4,
+        label: "Múa rối nước"
+    },
+    {
+        id: 5,
+        label: "Đua xe F1"
+    },
+];
