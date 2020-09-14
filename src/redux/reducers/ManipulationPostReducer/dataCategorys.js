065309517a882c6614eff81dcbfc5842893050ce
LeHieu <Xavan912@gmail.com>
2020-09-14 13:56:48 +0700

    Create dataCategorys.js
    Them du lieu danh muc

---
+++ src/redux/reducers/ManipulationPostReducer/dataCategorys.js
@@ -0,0 +1,19 @@
+
+export default [
+    {
+        value: '0',
+        label: 'Chọn danh mục',
+      },
+      {
+        value: '1',
+        label: 'Sức khỏe',
+      },
+      {
+        value: '2',
+        label: 'Du lịch',
+      },
+      {
+        value: '3',
+        label: 'Khoa học',
+      },
+];
