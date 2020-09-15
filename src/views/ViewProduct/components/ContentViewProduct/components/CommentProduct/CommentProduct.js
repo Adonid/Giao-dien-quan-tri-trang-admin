0033a6f55a44d7c75d644e9e26264520752119de
LeHieu <Xavan912@gmail.com>
2020-09-15 09:22:12 +0700

    Create CommentProduct.js
    Giao dien Comment

---
+++ src/views/ViewProduct/components/ContentViewProduct/components/CommentProduct/CommentProduct.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { 
+    Card, 
+    CardContent,
+    Grid,
+    CardHeader,
+    Divider
+    
+ } from '@material-ui/core';
+
+const CommentProduct = props => {
+
+    const { className, ...rest } = props;
+
+    return (
+        <Grid item xs={12} sm={8}>
+                    
+            <Card
+                {...rest}
+            >
+                <CardHeader title="Giới thiệu bài viết" />
+                <Divider/>
+                <CardContent>
+
+                </CardContent>
+            </Card>
+        </Grid>
+    );
+};
+
+CommentProduct.propTypes = {
+    
+};
+
+export default CommentProduct;
