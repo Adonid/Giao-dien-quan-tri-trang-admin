ab213f1fce9d7125e7be82517863ed5d45fc8caf
LeHieu <Xavan912@gmail.com>
2020-09-15 08:45:35 +0700

    Update ToolBarViewProduct.js
    Khoi tao giao dien

---
+++ src/views/ViewProduct/components/ToolBarViewProduct/ToolBarViewProduct.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Link as RouterLink
+} from "react-router-dom";
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/styles';
+import { 
+  Button, 
+  Typography,
+  Breadcrumbs,
+  Link
+ } from '@material-ui/core';
+ import BallotOutlinedIcon from '@material-ui/icons/BallotOutlined';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  row: {
+    height: '42px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: theme.spacing(0.5)
+    },
+  groupButton: {
+    marginBottom: theme.spacing(2),
+    marginTop: theme.spacing(2),
+    '& button': {
+      marginRight: theme.spacing(1),
+    }
+  },
+  searchInput: {
+    marginRight: theme.spacing(1)
+  }
+}));
+
+const ToolBarViewProduct = props => {
+
+  const { className, ...rest } = props;
+
+  const classes = useStyles();
+
+  return (
+    <div
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+      <div className={classes.row}>
+        <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb" gutterBottom>
+          <Link color="inherit" component={RouterLink} to="/dashboard">
+              Dashboard
+          </Link>
+          <Link color="inherit" component={RouterLink} to="/products">
+              Quản lý bài viết
+          </Link>
+          <Typography color="textPrimary">Một sô phương pháp phòng chống ho cho bé</Typography>
+        </Breadcrumbs>
+        <Link color="inherit" underline="none" component={RouterLink} to="/products">
+          <Button
+            color="primary"
+            variant="contained"
+          >
+            <BallotOutlinedIcon/> Sửa bài
+          </Button>
+        </Link>
+      </div>
+      <div>
+        <Typography variant="h3" gutterBottom>
+          Một sô phương pháp phòng chống ho cho bé
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+ToolBarViewProduct.propTypes = {
+  className: PropTypes.string
+};
+
+export default ToolBarViewProduct;
