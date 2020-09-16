5ed686ba119d700596e26f62f0a57ac312cc7f21
LeHieu <Xavan912@gmail.com>
2020-09-16 10:09:46 +0700

    Update index.js
    Them component chua cac quyen
    - Dung dang bai
    - Xoa bai

--- src/views/ViewProduct/components/ContentViewProduct/components/index.js
+++ src/views/ViewProduct/components/ContentViewProduct/components/index.js
@@ -1,3 +1,4 @@
 export { default as FixedContent } from './FixedContent';
 export { default as CommentProduct } from './CommentProduct';
 export { default as ProductRelative } from './ProductRelative';
+export { default as LimitProduct } from './LimitProduct';
