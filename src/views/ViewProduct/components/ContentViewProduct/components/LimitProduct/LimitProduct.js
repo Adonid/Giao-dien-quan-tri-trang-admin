d151b3d5416e2f8e68c1529c3c46f3c25be9ae9d
LeHieu <Xavan912@gmail.com>
2020-09-16 10:40:59 +0700

    Update LimitProduct.js
    Dua vao cac tac vu xac thuc de thuc thi

--- src/views/ViewProduct/components/ContentViewProduct/components/LimitProduct/LimitProduct.js
+++ src/views/ViewProduct/components/ContentViewProduct/components/LimitProduct/LimitProduct.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles, ThemeProvider } from '@material-ui/styles';
 import { 
     Typography, 
     Button,
@@ -8,10 +9,50 @@ import {
     CardHeader,
     Divider,
     Box,
-    CardContent
+    CardContent,
+    createMuiTheme
+
 } from '@material-ui/core';
+import { ConfirmDialog } from 'alerts';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+import NotInterestedIcon from '@material-ui/icons/NotInterested';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      padding: theme.spacing(3)
+    },
+    textWarning: {
+        paddingBottom: theme.spacing(1),
+        '& >p' : {
+          color: '#546e7ad9'
+        }
+      },
+  }));
+
+  const themeButtonDelete = createMuiTheme({
+    palette: {
+      primary : {
+        main: '#f44336',
+        contrastText: '#fff',
+      },
+    },
+  });
 
 const LimitProduct = props => {
+
+    const { isStop, closePost, distroyPost, openPost, ...rest } = props;
+
+    const classes = useStyles();
+
+    const [ openBlock, setOpenBlock ] = useState(false);
+    const [ openOpen, setOpenOpen ] = useState(false);
+    const [ openDistroy, setOpenDistroy ] = useState(false);
+
+    const handleBlocking = () => closePost({id: props.userinfo, name: props.userinfo.name});
+    const handleOpening = () => openPost({id: props.userinfo, name: props.userinfo.name});
+    const handleDistroyPost = () => distroyPost({id: props.userinfo, name: props.userinfo.name});
+
     return (
         <React.Fragment>
             <Grid 
@@ -20,31 +61,31 @@ const LimitProduct = props => {
                 sm={4}
             >
                 <Card>
-                    <CardHeader title="Hạn chế người dùng" />
+                    <CardHeader title="Hạn chế bài viết" />
                     <Divider/>
                     <CardContent>
                         <Box>
                             {
-                            props.userinfo.isClosed 
+                            true
                             ?
                             <Button 
                                 startIcon={ <LockOpenIcon fontSize="small" />}
                                 onClick={ ()=>setOpenOpen(!openOpen) }
                             >
-                                MỞ TÀI KHOẢN
+                                ĐĂNG LẠI BÀI
                             </Button>
                             :
                             <Button 
                                 startIcon={ <NotInterestedIcon fontSize="small" />}
                                 onClick={ ()=>setOpenBlock(!openBlock) }
                             >
-                                ĐÓNG TÀI KHOẢN
+                                DỪNG ĐĂNG BÀI
                             </Button>
                             }
                         </Box>
                         <Box className={ classes.textWarning}>
                             <Typography variant="body2" color="textSecondary">
-                                Xóa toàn bộ dữ liệu của người dùng, nếu không chắc chắn bạn hãy chọn đóng tài khoản vì điều này sẽ xóa không trở lại.
+                                Xóa toàn bộ dữ liệu của bài viết, nếu không chắc chắn bạn hãy chọn dừng đăng bài vì điều này sẽ xóa không trở lại.
                             </Typography>
                         </Box>
                         <ThemeProvider theme={themeButtonDelete}>
@@ -54,21 +95,24 @@ const LimitProduct = props => {
                                 startIcon={ <DeleteOutlinedIcon fontSize="small" />}
                                 onClick={ ()=>setOpenDistroy(!openDistroy) }
                             >
-                                XÓA TÀI KHOẢN
+                                XÓA BÀI VIẾT
                             </Button>
                         </ThemeProvider>
                     </CardContent>
                 </Card>
             </Grid>
-            <ConfirmDialog action={ handleBlocking } openDialog={openBlock} content={{type:'block', title:'Đóng tài khoản người dùng', note:`Tài khoản người dùng ${ props.userinfo.name } sẽ bị vô hiệu hóa cho đến khi bạn cho phép kích hoạt trở lại. Đóng tài khoản?`}} />
-            <ConfirmDialog action={ handleOpening } openDialog={openOpen} content={{type:'open', title:'Mở khóa tài khoản người dùng', note:`Tài khoản người dùng ${ props.userinfo.name } sẽ được phép hoạt động trở lại. Mở khóa cho tài khoản?`}} />
-            <ConfirmDialog action={ handleDistroyUser } openDialog={openDistroy} content={{type:'delete', title:'Xóa vĩnh viễn tài khoản người dùng', note:`Tài khoản ${ props.userinfo.name } sẽ bị xóa hoàn toàn trên hệ thống, thực thi sẽ không khôi phục được. Bạn có chắc?`}}/>
+            <ConfirmDialog action={ handleBlocking } openDialog={openBlock} content={{type:'block', title:'Dừng đăng bài', note:`Bài viết sẽ mất quyền phê duyệt? Bạn có thể cấp lại quyền phê duyệt để đăng bài trở lại.`}} />
+            <ConfirmDialog action={ handleOpening } openDialog={openOpen} content={{type:'open', title:'Đăng bài trở lại', note:`Bài viết sẽ được cấp lại quyền phê duyệt và tự động được đăng?`}} />
+            <ConfirmDialog action={ handleDistroyPost } openDialog={openDistroy} content={{type:'delete', title:'Xóa dữ liệu bài viết', note:`Bài viết sẽ bị xóa hoàn toàn trên hệ thống, thực thi sẽ không khôi phục được. Bạn có chắc?`}}/>
         </React.Fragment>
     );
 };
 
 LimitProduct.propTypes = {
-    
+    isStop: PropTypes.bool.isRequired,
+    closePost: PropTypes.func.isRequired,
+    openPost: PropTypes.func.isRequired,
+    distroyPost: PropTypes.func.isRequired,
 };
 
 export default LimitProduct;
