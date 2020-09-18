906a256c6e0c6f77ece7fce215c1951d36f3d74c
LeHieu <Xavan912@gmail.com>
2020-09-18 09:44:01 +0700

    Create dataComment.js
    Them component chi tiet bai viet reducer

---
+++ src/redux/reducers/PostDetailReducer/dataComment.js
@@ -0,0 +1,77 @@
+export default [
+    {
+        id: 1,
+        name: "Alec Thompson",
+        time: "7 minutes ago",
+        avatar: "https://demos.creative-tim.com/material-kit-pro-react/static/media/card-profile1-square.3122abf4.jpg",
+        content: "I've been trying to figure out the bed design for the master bedroom at our Hidden Hills compound...I like good music from Youtube.",
+        favourite: [
+            {
+                id: 2,
+                name: "Thành Trung",
+            },
+            {
+                id: 5,
+                name: "Cẩm Ly",
+            },
+            {
+                id: 8,
+                name: "Quang Trung",
+            },
+            {
+                id: 19,
+                name: "Sơn Ca",
+            },
+        ],
+        replyComments: [],
+        openReply: false,
+        meVote: false,
+    },
+    {
+        id: 2,
+        name: "Tina Andrew",
+        time: "19 minutes ago",
+        avatar: "https://demos.creative-tim.com/material-kit-pro-react/static/media/card-profile4-square.1a164917.jpg",
+        content: "Hello guys, nice to have you on the platform! There will be a lot of great stuff coming soon. We will keep you posted for the latest news. Don't forget, You're Awesome!",
+        favourite: [
+            {
+                id: 6,
+                name: "Văn Quang",
+            },
+            {
+                id: 9,
+                name: "Lê Đình Chiến",
+            },
+            {
+                id: 1,
+                name: "Phạm Bách",
+            },
+        ],
+        replyComments: [
+            {
+                id: 5,
+                name: "Tina Andrew",
+                time: "10 minutes ago",
+                avatar: "https://demos.creative-tim.com/material-kit-pro-react/static/media/card-profile6-square.1f1f4900.jpg",
+                content: "Chance too good. God level bars. I'm so proud of @LifeOfDesiigner #1 song in the country. Panda! Don't be scared of the truth because we need to restart the human foundation in truth I stand with the most humility. We are so blessed!",
+                favourite: [
+                    {
+                        id: 3,
+                        name: "Phan Giang",
+                    },
+                    {
+                        id: 11,
+                        name: "Đại Tửu",
+                    },
+                    {
+                        id: 15,
+                        name: "Vương Hồng",
+                    },
+                ],
+                meVote: false,
+            },
+        ],
+        openReply: false,
+        meVote: true,
+    }
+];
