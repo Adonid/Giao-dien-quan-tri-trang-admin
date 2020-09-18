183a4a13ab92cafcdb842127633098a9bf6b82d2
LeHieu <Xavan912@gmail.com>
2020-09-18 09:43:57 +0700

    Update index.js
    Them component chi tiet bai viet reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -5,3 +5,4 @@ export { default as UserEditorReducer  } from './UserEditorReducer';
 export { default as ChatsDataReducer  } from './ChatsDataReducer';
 export { default as ProductsListReducer  } from './ProductsListReducer';
 export { default as ManipulationPostReducer  } from './ManipulationPostReducer';
+export { default as PostDetailReducer  } from './PostDetailReducer';
