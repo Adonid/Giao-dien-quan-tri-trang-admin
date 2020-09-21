196b7dd41defc6fa6104ac7f67a1db41477e4d08
LeHieu <Xavan912@gmail.com>
2020-09-21 09:01:15 +0700

    Update FixedContent.js
    Hoan thien phan noi dung chi tiet bai

--- src/views/ViewProduct/components/ContentViewProduct/components/FixedContent/FixedContent.js
+++ src/views/ViewProduct/components/ContentViewProduct/components/FixedContent/FixedContent.js
@@ -176,12 +176,4 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-    return {
-        dispatch1: () => {
-            dispatch(actionCreator)
-        }
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(FixedContent)
+export default connect(mapStateToProps)(FixedContent)
