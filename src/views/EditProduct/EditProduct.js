03e317d7d412065c65394f11b8567638c5943c3e
LeHieu <Xavan912@gmail.com>
2020-09-21 09:34:04 +0700

    Create EditProduct.js
    Them trang sua bai viet

---
+++ src/views/EditProduct/EditProduct.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import { ProductsToolbar, ContentEditProduct } from './components';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      padding: theme.spacing(3)
+    },
+    content: {
+      marginTop: theme.spacing(2)
+    }
+  }));
+
+const EditProduct = props => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <ProductsToolbar />
+            <div className={classes.content}>
+                <ContentNewProduct />
+            </div>
+        </div>
+    );
+};
+
+EditProduct.propTypes = {
+    
+};
+
+export default EditProduct;
