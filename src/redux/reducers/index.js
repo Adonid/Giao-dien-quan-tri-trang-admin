79ba68ead059edc258d7caa567a6507332be7ba5
LeHieu <Xavan912@gmail.com>
2020-09-21 13:45:59 +0700

    Update index.js
    sua ten

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -5,4 +5,4 @@ export { default as UserEditorReducer  } from './UserEditorReducer';
 export { default as ChatsDataReducer  } from './ChatsDataReducer';
 export { default as ProductsListReducer  } from './ProductsListReducer';
 export { default as ManipulationPostReducer  } from './ManipulationPostReducer';
-export { default as PostDetailReducer  } from './PostDetailReducer';
+export { default as ProductEditReducer  } from './ProductEditReducer';
