888707b5caffa97a346383d93e8d00316fe57b9b
LeHieu <Xavan912@gmail.com>
2020-09-21 16:15:39 +0700

    Create data.js
    Dua vao du lieu demo viec sua bai viet

---
+++ src/redux/reducers/ProductEditReducer/data.js
@@ -0,0 +1,87 @@
+export default {
+        id: 1,
+        categorys: [
+            {
+                id: 2,
+                label: "Du lịch",
+                image: "https://images.unsplash.com/photo-1589030446232-da1800eb736a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                select: true
+            },
+            {
+                id: 5,
+                label: "Kinh tế",
+                image: "https://images.unsplash.com/photo-1561414927-6d86591d0c4f?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+                select: false
+            },
+            {
+                id: 7,
+                label: "Đời sống",
+                image: "https://images.unsplash.com/photo-1523240795612-9a054b0db644?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                select: false
+            },
+            {
+                id: 3,
+                label: "Văn hóa",
+                image: "https://images.unsplash.com/photo-1508598126084-9dfccbb92cd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                select: false
+            },
+            {
+                id: 1,
+                label: "Ẩm thực",
+                image: "https://images.unsplash.com/photo-1528605248644-14dd04022da1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                select: false
+            },
+        ],
+        title: "Mùa vàng trong nắng thu",
+        description: "Tháng 9 - 10, những thửa ruộng bậc thang chín vàng óng ánh tại Yên Bái và Cao Bằng đẹp tựa bức họa vùng cao níu chân lữ khách.",
+        image: "https://i1-dulich.vnecdn.net/2020/09/14/7-tu-le-yen-bai-pham-chi-cong-1600064577-1600064597.jpg?w=900&h=540&q=100&dpr=1&fit=crop&s=RvcbtGEtK1rYBblZ13lEZQ",
+        tags: [
+            {
+                id: 1,
+                label: "Đời sống",
+                select: true
+            },
+            {
+                id: 2,
+                label: "Châu Âu",
+                select: false
+            },
+            {
+                id: 3,
+                label: "Đại dương",
+                select: false
+            },
+            {
+                id: 4,
+                label: "Châu Á",
+                select: true
+            },
+            {
+                id: 9,
+                label: "Món ngon",
+                select: true
+            },
+        ],
+        content: `
+            <div class="MuiGrid-root jss159  MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-8 MuiGrid-grid-md-8">
+            <h3 class="jss347">The Castle Looks Different at Night...</h3>
+            <p>This is the paragraph where you can write more details about your product. Keep you user engaged by providing meaningful information. Remember that by this time, the user is curious, otherwise he wouldn't scroll to get here. Add a button if you want the user to see more. We are here to make life better.<br /><br />And now I look and look around and there&rsquo;s so many Kanyes I've been trying to figure out the bed design for the master bedroom at our Hidden Hills compound... and thank you for turning my personal jean jacket into a couture piece.</p>
+            <blockquote class="jss359 jss361">
+            <p class="jss362 jss349">&ldquo;And thank you for turning my personal jean jacket into a couture piece.&rdquo;</p>
+            <small class="jss363">Kanye West, Producer.</small></blockquote>
+            </div>
+            <div class="MuiGrid-root jss159 jss348 MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-10 MuiGrid-grid-md-10">
+            <div class="MuiGrid-root jss158  MuiGrid-container">
+            <div class="MuiGrid-root jss159  MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-4 MuiGrid-grid-md-4"><img class="jss353 jss351 jss350" src="https://demos.creative-tim.com/material-kit-pro-react/static/media/blog4.5de2130f.jpg" alt="..." /></div>
+            <div class="MuiGrid-root jss159  MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-4 MuiGrid-grid-md-4"><img class="jss353 jss351 jss350" src="https://demos.creative-tim.com/material-kit-pro-react/static/media/blog3.a4ee46b8.jpg" alt="..." /></div>
+            <div class="MuiGrid-root jss159  MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-4 MuiGrid-grid-md-4"><img class="jss353 jss351 jss350" src="https://demos.creative-tim.com/material-kit-pro-react/static/media/blog1.9313c5c1.jpg" alt="..." /></div>
+            </div>
+            </div>
+            <div class="MuiGrid-root jss159  MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-8 MuiGrid-grid-md-8">
+            <h3 class="jss347">Rest of the Story:</h3>
+            <p>We are here to make life better. And now I look and look around and there&rsquo;s so many Kanyes I've been trying to figure out the bed design for the master bedroom at our Hidden Hills compound... and thank you for turning my personal jean jacket into a couture piece.<br />I speak yell scream directly at the old guard on behalf of the future. daytime All respect prayers and love to Phife&rsquo;s family Thank you for so much inspiration.</p>
+            <p>Thank you Anna for the invite thank you to the whole Vogue team And I love you like Kanye loves Kanye Pand Pand Panda I've been trying to figure out the bed design for the master bedroom at our Hidden Hills compound...The Pablo pop up was almost a pop up of influence. All respect prayers and love to Phife&rsquo;s family Thank you for so much inspiration daytime I love this new Ferg album! The Life of Pablo is now available for purchase I have a dream. Thank you to everybody who made The Life of Pablo the number 1 album in the world! I'm so proud of the nr #1 song in the country. Panda! Good music 2016!</p>
+            <p>I love this new Ferg album! The Life of Pablo is now available for purchase I have a dream. Thank you to everybody who made The Life of Pablo the number 1 album in the world! I'm so proud of the nr #1 song in the country. Panda! Good music 2016!</p>
+            </div>
+        `,
+}
