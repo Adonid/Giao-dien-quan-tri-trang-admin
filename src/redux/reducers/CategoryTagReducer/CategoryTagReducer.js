20a44a28db8370e01c33fc8c4dcfb85498af3462
LeHieu <Xavan912@gmail.com>
2020-09-25 08:34:09 +0700

    Update CategoryTagReducer.js
    Hoan thien category manipulation

--- src/redux/reducers/CategoryTagReducer/CategoryTagReducer.js
+++ src/redux/reducers/CategoryTagReducer/CategoryTagReducer.js
@@ -20,12 +20,24 @@ const CategoryTagReducer = (state = dataCategoryTag, action) => {
         case 'DELETE_CAT':
             const idCatDel = action.idCat;
             /** api xoa 1 category */
-
+                // chu y dua so luong bai viet thuoc danh muc nay ve danh muc mac dinh truoc khi xoa
             /** end */
             // demo sau khi api xong
             state = {...state, categorys: [...state.categorys].filter( cat => cat.id !== idCatDel ) };
             return state;
 
+        
+        case 'UPDATE_CAT':
+            const dataUpdateCat = action.dataCatUpdate;
+            /** api sua 1 category */
+                // dataUpdateCat: object { id: int, label: string, avatar: string }
+                // Chu y kiem tra avarta: neu la base64 thi phai luu anh, neu la url anh thi chi can luu lai, neu rong thi luu rong
+            /** end */
+            // demo sau khi api xong
+            state = {...state, categorys: [...state.categorys].map( cat => cat.id===dataUpdateCat.id ? dataUpdateCat : cat) };
+            return state;
+
+        
         default:
             return state
     }
