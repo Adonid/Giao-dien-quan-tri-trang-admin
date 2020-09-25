318dff4bf92a9002b74e98a62b82ce9819b5249f
LeHieu <Xavan912@gmail.com>
2020-09-25 08:12:14 +0700

    Update CategoryTagReducer.js
    Xong xu ly xoa category

--- src/redux/reducers/CategoryTagReducer/CategoryTagReducer.js
+++ src/redux/reducers/CategoryTagReducer/CategoryTagReducer.js
@@ -16,6 +16,15 @@ const CategoryTagReducer = (state = dataCategoryTag, action) => {
             // demo sau khi api xong
             state = {...state, categorys: [...state.categorys, { id: Math.floor(Math.random() * (99999 - 99)) + 99, label: nameCat, qtyProducts: 0 }]};
             return state;
+        
+        case 'DELETE_CAT':
+            const idCatDel = action.idCat;
+            /** api xoa 1 category */
+
+            /** end */
+            // demo sau khi api xong
+            state = {...state, categorys: [...state.categorys].filter( cat => cat.id !== idCatDel ) };
+            return state;
 
         default:
             return state
