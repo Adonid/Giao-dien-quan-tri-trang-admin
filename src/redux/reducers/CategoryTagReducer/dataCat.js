ca54c6d14c507036c1dd56b54e3253154676df4b
LeHieu <Xavan912@gmail.com>
2020-09-25 07:48:25 +0700

    Update dataCat.js
    Dua du lieu demo vao

---
+++ src/redux/reducers/CategoryTagReducer/dataCat.js
@@ -0,0 +1,32 @@
+export default [
+    { 
+        id: 1, 
+        label: 'Angular', 
+        qtyProducts: 12, 
+        avatar: "https://images.unsplash.com/photo-1600800609386-11ab3105a9e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
+    },
+    { 
+        id: 2, 
+        label: 'jQuery', 
+        qtyProducts: 34, 
+        avatar: "https://images.unsplash.com/photo-1556691432-33035a3466fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
+    },
+    { 
+        id: 3, 
+        label: 'Polymer', 
+        qtyProducts: 77, 
+        avatar: "https://images.unsplash.com/photo-1597690310639-272d222023b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
+    },
+    { 
+        id: 4, 
+        label: 'React', 
+        qtyProducts: 72, 
+        avatar: "https://images.unsplash.com/photo-1551835503-ffa9edbacc75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
+    },
+    { 
+        id: 5, 
+        label: 'Vue.js', 
+        qtyProducts: 90, 
+        avatar: "https://images.unsplash.com/photo-1517137660927-27542f89984d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
+    },
+  ];
