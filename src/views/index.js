045d63078b371a207fc17bcf915a0d2a8fed59cf
LeHieu <Xavan912@gmail.com>
2020-09-25 16:18:04 +0700

    Update index.js
    Xoa component ko can thiet

--- src/views/index.js
+++ src/views/index.js
@@ -8,7 +8,6 @@ export { default as SignIn } from './SignIn';
 export { default as SignUp } from './SignUp';
 export { default as ForeverPassword } from './ForeverPassword';
 export { default as ResetPassword } from './ResetPassword';
-export { default as CreateAccountSuccess } from './CreateAccountSuccess';
 export { default as Typography } from './Typography';
 export { default as UserList } from './UserList';
 export { default as UserDetail } from './UserDetail';
