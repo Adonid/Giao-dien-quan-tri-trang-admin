b6e657fcc97a543532d6cf819863da25c211dbd3
LeHieu <Xavan912@gmail.com>
2020-09-27 08:32:29 +0700

    Update AlertMiniPageReducer.js
    Xu ly dang ki va dua ra thong bao

--- src/redux/reducers/AlertMiniPageReducer/AlertMiniPageReducer.js
+++ src/redux/reducers/AlertMiniPageReducer/AlertMiniPageReducer.js
@@ -1,11 +1,26 @@
 
-const dataAlertMiniPage = {}
+const dataAlertMiniPage = {
+    signUpCompleted: {
+        type    : "info",
+        content : "Hello Word"
+    },
+    resetPasswordCompleted: {
+        type    : "info",
+        content : "Hello Word"
+    },
+}
 
 const AlertMiniPageReducer = (state = dataAlertMiniPage, action) => {
     switch (action.type) {
         case 'SIGNUP_COMPLETED':
             const dataUserSignUp = action.data;
-            console.log(dataUserSignUp);
+            const historySignUp = action.history;
+            /** api tao moi account */
+                // dataUserSignUp: object { email: string firstName: string password: string phoneNumber: number policy: bool }
+            /** end */
+            // vi du sau khi api
+            historySignUp.push('/sign-in');
+            state = { ...state, signUpCompleted: {type: "success", content: "Đăng ký thành công! Vui lòng xác thực tài khoản qua email đã gửi."}}
             return state;
 
         default:
