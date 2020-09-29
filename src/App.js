256d7d73d54bcfcfb3a6f5a028bdf9aa58dc9edf
LeHieu <Xavan912@gmail.com>
2020-09-29 16:11:59 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ import './assets/scss/index.scss';
 import validators from './common/validators';
 import Routes from './Routes';
 
-import {FirebaseConnect} from './firebase';
-
 const browserHistory = createBrowserHistory();
 
 Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
@@ -27,7 +25,6 @@ validate.validators = {
 
 export default class App extends Component {
   render() {
-    console.log(FirebaseConnect);
     return (
       <ThemeProvider theme={theme}>
         <Router history={browserHistory}>
