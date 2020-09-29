aab1bc4e75eae7c598685640de038a36477a5013
LeHieu <Xavan912@gmail.com>
2020-09-29 16:43:53 +0700

    Update FirebaseConnect.js
    Thay cach viet bang cach dua file .env vao project

--- src/firebase/FirebaseConnect/FirebaseConnect.js
+++ src/firebase/FirebaseConnect/FirebaseConnect.js
@@ -1,18 +1,14 @@
 import * as firebase from 'firebase/app';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyAK58wcil5ObBO7FwkPCbIG8hIYRct10Lo",
-    authDomain: "react-blog-admin.firebaseapp.com",
-    databaseURL: "https://react-blog-admin.firebaseio.com",
-    projectId: "react-blog-admin",
-    storageBucket: "react-blog-admin.appspot.com",
-    messagingSenderId: "1092013769006",
-    appId: "1:1092013769006:web:eaac28db8f277393b1babf",
-    measurementId: "G-SVSNJ1RE3N"
-  };
-  // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
-//   firebase.analytics();
-const FirebaseConnect = firebase.initializeApp(firebaseConfig);
+const FirebaseConnect = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMIAN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+})
 
 export default FirebaseConnect;
