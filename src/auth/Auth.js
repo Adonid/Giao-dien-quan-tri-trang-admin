08bc861fde4fa783880b2cdf4aa79a19d98ec2ac
LeHieu <Xavan912@gmail.com>
2020-09-30 14:43:38 +0700

    Update Auth.js

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -1,26 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import {FirebaseConnect} from 'firebaseConnect';
-
+import React, { useEffect, useState } from "react";
+import base from "base.js";
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
-    
-    const [ currentUser, setCurrentUser ] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [pending, setPending] = useState(true);
+  useEffect(() => {
+    base.auth().onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setPending(false);
+    });
+  }, []);
 
-    useEffect( () => {
-        FirebaseConnect.auth().onAuthStateChanged( setCurrentUser );
-    },[]);
+  if(pending){
+    return <>Loading...</>
+  }
 
-    return (
-        <AuthContext.Provider
-            value={{currentUser}}
-        >
-            {children}
-        </AuthContext.Provider>
-    );
-};
-
-AuthProvider.propTypes = {
-    children: PropTypes.object
+  return (
+    <AuthContext.Provider
+      value={{
+        currentUser
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
