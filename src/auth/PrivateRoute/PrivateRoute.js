f686e43eed92c0bfba27692ebf4ef8e4aa4f7408
LeHieu <Xavan912@gmail.com>
2020-09-30 10:05:22 +0700

    Create PrivateRoute.js
    Tao route bao mat

---
+++ src/auth/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+import { AuthContext } from 'auth/Auth'
+
+const PrivateRoute = props => {
+    const { layout: Layout, component: RouteComponent, ...rest } = props;
+    const { currentUser } = useContext( AuthContext );
+    return (
+        <Route
+        {...rest}
+        render={routeProps => (
+            !!currentUser
+            ?
+                <Layout>
+                    <RouteComponent {...routeProps} />
+                </Layout>
+            :
+                <Redirect to="/sign-in"/>
+        )}
+        />
+    );
+};
+
+PrivateRoute.propTypes = {
+    component: PropTypes.any.isRequired,
+    layout: PropTypes.any.isRequired,
+    path: PropTypes.string
+};
+
+export default PrivateRoute;
