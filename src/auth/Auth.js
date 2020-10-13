be72677d67d1e1efd4c1e8b53453d579c3b4c7ac
LeHieu <Xavan912@gmail.com>
2020-10-13 10:34:19 +0700

    Update Auth.js

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -1,24 +1,17 @@
 import React, { useEffect, useState } from "react";
 import base from "base.js";
-import { connect } from "react-redux";
-import PropTypes from 'prop-types';
-
-
 export const AuthContext = React.createContext();
 
-export const AuthProvider = ({ children, __Sucure_user }) => {
+export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const [pending, setPending] = useState(true);
-  const [sucure, setSucure] = useState(null);
   useEffect(() => {
-    console.log(__Sucure_user);
     base.auth().onAuthStateChanged((user) => {
       setCurrentUser(user);
       setPending(false);
     });
     // Thuc ra la kiem tra token trong cookie xem co hop le hay khong
-  }, [__Sucure_user]);
-  
+  }, []);
 
   if(pending){
     return <>Loading...</>
@@ -34,16 +27,3 @@ export const AuthProvider = ({ children, __Sucure_user }) => {
     </AuthContext.Provider>
   );
 };
-
-AuthProvider.propTypes = {
-  children: PropTypes.node.isRequired,
-  __Sucure_user: PropTypes.object.isRequired
-}
-
-const mapStateToProps = (state, ownProps) => {
-  return {
-    __Sucure_user: state.dataAlertMiniPage.sigIn.token
-  }
-}
-
-export default connect(mapStateToProps, null)(AuthProvider)
