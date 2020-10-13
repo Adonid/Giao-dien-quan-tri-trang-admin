888a6dfd026a5ea757747ae1e4ce8998ce2e426a
LeHieu <Xavan912@gmail.com>
2020-10-13 08:23:01 +0700

    Update SignIn.js
    Khong su dung cookie
    Dung store

--- src/views/SignIn/SignIn.js
+++ src/views/SignIn/SignIn.js
@@ -212,6 +212,24 @@ const SignIn = props => {
     setLoading(false);
   }
 
+  // const ReadCookie = () =>
+  //   {
+  //       var allcookies = window.document.cookie;
+
+  //       // Get all the cookies pairs in an array
+  //       var cookiearray = allcookies.split(';');
+
+  //       // Now take key value pair out of this array
+  //       for(var i=0; i<cookiearray.length; i++){
+  //         let name = cookiearray[i].split('=')[0];
+  //         let value = cookiearray[i].split('=')[1];
+  //         if(name==="__Sucure_user")
+  //           return value;
+  //         else
+  //           return null;
+  //       }
+  //   }
+
   if (currentUser) {
     return <Redirect to="/dashboard" />;
   }
