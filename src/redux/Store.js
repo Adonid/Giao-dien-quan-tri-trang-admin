ab123321efc2fc1dac0a93fcfe2d82cfd1db7bb3
LeHieu <Xavan912@gmail.com>
2020-10-14 14:57:51 +0700

    Update Store.js
    Dua reducer vao store

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -14,6 +14,7 @@ import {
     CategoryTagReducer as CategoryTag,
     TermReducer as Term,
     AlertMiniPageReducer as AlertMiniPage,
+    LoginReducer as Login,
 
 } from './reducers';
 
@@ -32,6 +33,7 @@ var allReducers = combineReducers({
     dataCategoryTag: CategoryTag,
     dataTerm: Term,
     dataAlertMiniPage: AlertMiniPage,
+    dataLogin: Login,
 });
 
 const Store = createStore(allReducers, applyMiddleware(...middleware, logger));
