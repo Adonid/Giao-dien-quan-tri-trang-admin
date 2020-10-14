aa23ee6f2f4818bc0423a02de45bce497c19f09c
LeHieu <Xavan912@gmail.com>
2020-10-14 08:50:52 +0700

    Update PrivateRoute.js

--- src/auth/PrivateRoute/PrivateRoute.js
+++ src/auth/PrivateRoute/PrivateRoute.js
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
-import { AuthContext } from 'auth/Auth'
+import { AuthContext } from 'auth/Auth';
 
 const PrivateRoute = props => {
     const { layout: Layout, component: RouteComponent, ...rest } = props;
     const { currentUser } = useContext( AuthContext );
+
     return (
         <Route
         {...rest}
