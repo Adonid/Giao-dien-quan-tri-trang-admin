cf73818224142ef5f0009425e90ddead731ac266
LeHieu <Xavan912@gmail.com>
2020-10-15 14:58:35 +0700

    Create Axios.js
    Config lai axios cac hang so co ban

---
+++ src/common/Axios.js
@@ -0,0 +1,24 @@
+import Axios from 'axios';
+import axios from 'axios';
+
+const readCookie = () =>
+{
+    var allcookies = window.document.cookie;
+
+    // Get all the cookies pairs in an array
+    var cookiearray = allcookies.split(';');
+
+    // Now take key value pair out of this array
+    for(var i=0; i<cookiearray.length; i++){
+        let name = cookiearray[i].split('=')[0];
+        let value = cookiearray[i].split('=')[1];
+        if(name==="__Sucure_user" && value)
+            return value;
+    }
+    return null;
+}
+
+axios.defaults.baseURL = 'https://us-central1-nodejs-firebase-cloud-func.cloudfunctions.net/api/';
+axios.defaults.headers.common['authorization'] = `Bearer ${readCookie()}`;
+
+export default axios;
