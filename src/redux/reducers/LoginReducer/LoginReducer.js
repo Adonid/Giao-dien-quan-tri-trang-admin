ce8046d9658fd3ed26fc7edcbeea5cf7cf207144
LeHieu <Xavan912@gmail.com>
2020-10-15 14:19:13 +0700

    Update LoginReducer.js
    Dua vao trang thai bat dau xu ly middleware

--- src/redux/reducers/LoginReducer/LoginReducer.js
+++ src/redux/reducers/LoginReducer/LoginReducer.js
@@ -1,7 +1,8 @@
-import { LOGIN_ADMIN_SUCCESS, LOGIN_ADMIN_ERROR } from "redux/constans";
+import { LOGIN_ADMIN_SUCCESS, LOGIN_ADMIN_ERROR, LOGIN_ADMIN } from "redux/constans";
 
 const loginData = {
     enable: false,
+    loading: false,
     info: {}
 }
 
@@ -9,13 +10,16 @@ const LoginReducer = (state = loginData, action) => {
     
     switch (action.type) {
         
+        case LOGIN_ADMIN:
+            return { ...state, loading: true };
+
         case LOGIN_ADMIN_SUCCESS:
             window.document.cookie = "__Sucure_user=" + action.payload.sucure;
-            return { ...state, enable: action.payload.logged };
+            return { ...state, enable: action.payload.logged, loading: false };
         
         case LOGIN_ADMIN_ERROR:
             window.document.cookie = "__Sucure_user=";
-            return { ...state, enable: action.payload.logged };
+            return { ...state, enable: action.payload.logged, loading: false };
 
         default:
             return state
