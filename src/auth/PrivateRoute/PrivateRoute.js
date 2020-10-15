e9ae550ba23e94046534f3757c3dbb021d0f6e7e
LeHieu <Xavan912@gmail.com>
2020-10-15 10:21:00 +0700

    Update PrivateRoute.js
    Optimus

--- src/auth/PrivateRoute/PrivateRoute.js
+++ src/auth/PrivateRoute/PrivateRoute.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
