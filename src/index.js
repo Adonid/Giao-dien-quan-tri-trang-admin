76e7a50ffaffaf5d814dd84d9331f37e03428323
LeHieu <Xavan912@gmail.com>
2020-10-15 09:08:23 +0700

    Update index.js
    Cai dat them goi giu trang thai state khi refresh lai trang

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { PersistGate } from 'redux-persist/integration/react'
 import * as serviceWorker from './serviceWorker';
 import App from './App';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import Store from './redux';
+import { persistStore } from 'redux-persist';
+
+var persistor = persistStore(Store);
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={ Store }>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
     </React.StrictMode>
     , document.getElementById('root')
