6a3b2487b2342c5daf248e1ab060a9d9232784a4
LeHieu <Xavan912@gmail.com>
2020-10-16 13:31:38 +0700

    Update Logout.js
    Bo qua goi api

--- src/redux/actions/Logout/Logout.js
+++ src/redux/actions/Logout/Logout.js
@@ -1,32 +1,14 @@
-import axios from 'common/Axios';
 import { LOGOUT_ADMIN } from 'redux/constans';
 
-const Login = () => async dispatch => {
-    
-    try{
-        const res = await axios({
-            method: 'GET',
-            url: 'logout',
-            headers: {'X-Requested-With': 'XMLHttpRequest'},
-            });
-        dispatch( {
-            type: LOGOUT_ADMIN,
-            payload: {
-                message: "Đăng nhập để vào phiên làm việc mới!",
-                logged: false,
-            }
-        });
-    }
-    catch(e){
-        dispatch( {
-            type: LOGOUT_ADMIN,
-            payload: {
-                message: "Đã có lỗi xảy ra! status: 500",
-                logged: true,
-            },
-        });
-    }
+const Logout = () => async dispatch => {
+    dispatch( {
+        type: LOGOUT_ADMIN,
+        payload: {
+            message: "Đăng nhập để vào phiên làm việc mới!",
+            logged: false,
+        }
+    });
 
 }
 
-export default Login;
+export default Logout;
