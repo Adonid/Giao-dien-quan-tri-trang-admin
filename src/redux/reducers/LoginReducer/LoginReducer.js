2d6b5c7803f5ed755b7a7f1063e878bfa6eda772
LeHieu <Xavan912@gmail.com>
2020-10-16 16:55:04 +0700

    Update LoginReducer.js
    Cac truong hop logout co the xay ra

--- src/redux/reducers/LoginReducer/LoginReducer.js
+++ src/redux/reducers/LoginReducer/LoginReducer.js
@@ -21,6 +21,11 @@ const LoginReducer = (state = loginData, action) => {
             window.document.cookie = "__Sucure_user=";
             return { ...state, enable: action.payload.logged, loading: false };
 
+        /** 
+         *  Truong hop logout nay dung trong: 
+         *  1. Nguoi dung bam vao logout
+         *  2. Khi thong tin headers gui len server bi sai va tra ve loi thi cung dua ve trang thai logout
+         *  */ 
         case LOGOUT_ADMIN:
             window.document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
             return { ...state, enable: action.payload.logged, info: action.payload.message, };
