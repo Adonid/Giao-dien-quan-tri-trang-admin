b948b408f39929187f98afdbfcb2d6761d9de75a
LeHieu <Xavan912@gmail.com>
2020-10-17 09:07:33 +0700

    Create ForgetPwReducer.js
    Xu ly quen mat khau

---
+++ src/redux/reducers/ForgetPwReducer/ForgetPwReducer.js
@@ -0,0 +1,28 @@
+import { FORGET_PASSWORD_SUCCESS, FORGET_PASSWORD_ERROR, FORGET_PASSWORD } from "redux/constans";
+
+const dataResetPassword = {
+    loading: false,
+    messege: {}
+}
+
+const LoginReducer = (state = dataResetPassword, action) => {
+    
+    switch (action.type) {
+        
+        case FORGET_PASSWORD:
+            return { ...state, loading: true };
+
+        case FORGET_PASSWORD_SUCCESS:
+            return { ...state, message: action.payload.message, loading: false };
+        
+        case FORGET_PASSWORD_ERROR:
+            return { ...state, message: action.payload.message, loading: false };
+
+        default:
+            return state
+    }
+}
+
+
+
+export default LoginReducer;
