454ae7fbfae4205d5449860b16a7eb7dd1ae7703
LeHieu <Xavan912@gmail.com>
2020-10-19 09:11:42 +0700

    Update index.js
    Lay thong tin profile cua admin

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,3 +1,4 @@
 export { default as Login } from './Login';
 export { default as Logout } from './Logout';
 export { default as ForgetPassword } from './ForgetPassword';
+export { default as AdminProfile } from './AdminProfile';
