9956c7973ae847249f491a32eafb6d0d9225d1b7
LeHieu <Xavan912@gmail.com>
2020-10-20 14:20:33 +0700

    Update Axios.js
    Tach ra thanh 1 ham doc cookie

--- src/common/Axios.js
+++ src/common/Axios.js
@@ -1,24 +1,10 @@
 import Axios from 'axios';
 import axios from 'axios';
+import ReadCookie from './ReadCookie';
 
-const readCookie = () =>
-{
-    var allcookies = window.document.cookie;
-
-    // Get all the cookies pairs in an array
-    var cookiearray = allcookies.split(';');
-
-    // Now take key value pair out of this array
-    for(var i=0; i<cookiearray.length; i++){
-        let name = cookiearray[i].split('=')[0];
-        let value = cookiearray[i].split('=')[1];
-        if(name==="__Sucure_user" && value)
-            return value;
-    }
-    return null;
-}
+const sucure = ReadCookie();
 
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL_API;
-axios.defaults.headers.common['authorization'] = `Bearer ${readCookie()}`;
+// axios.defaults.headers.common['Authorization'] = "Bearer" + sucure;
 
 export default axios;
