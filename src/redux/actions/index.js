396d6e3d159b327516133af4a04c66153d05963a
LeHieu <Xavan912@gmail.com>
2020-10-24 11:08:35 +0700

    Update index.js
    Them phuong thuc update anh avatar

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,3 +2,4 @@ export { default as Login } from './Login';
 export { default as Logout } from './Logout';
 export { default as ForgetPassword } from './ForgetPassword';
 export { default as AdminProfile } from './AdminProfile';
+export { default as UploadAvatar } from './UploadAvatar';
