bc06c8f5167ffdc2cdefa581081c0a5ef18c5d10
LeHieu <Xavan912@gmail.com>
2020-10-27 09:58:26 +0700

    Update index.js
    Lay thong tin ca nhan cua admin

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,3 +3,4 @@ export { default as Logout } from './Logout';
 export { default as ForgetPassword } from './ForgetPassword';
 export { default as AdminProfile } from './AdminProfile';
 export { default as UploadAvatar } from './UploadAvatar';
+export { default as AdminDetail } from './AdminDetail';
