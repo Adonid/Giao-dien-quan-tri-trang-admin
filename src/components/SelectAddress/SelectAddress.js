18b1b6c355c3f953bc651d0d4050e81b03047143
LeHieu <Xavan912@gmail.com>
2020-10-28 07:20:29 +0700

    Create SelectAddress.js
    Them component lua chon tinh/huyen/xa

---
+++ src/components/SelectAddress/SelectAddress.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+    TextField,
+    MenuItem
+  } from '@material-ui/core';
+
+const SelectAddress = props => {
+
+    const { label, list, valSelect, action, disable, margin, fullWidth, required } = props;
+
+    const [ itemSelect, setItemSlect ] = useState(valSelect);
+
+    const handleChange = event => {
+      event.persist();
+      const val = event.target.value;
+      setItemSlect(val);
+      action(val);
+    }
+
+    return (
+        <TextField
+            required={ required??false }
+            fullWidth={fullWidth??false}
+            id="outlined-select-item"
+            select
+            margin={ margin??"none" }
+            label={label??"Sắp xếp"}
+            value={ itemSelect??0 }
+            onChange={handleChange}
+            // helperText="Thứ tự sắp xếp"
+            variant="outlined"
+            disabled={ disable??false}
+        >
+            {
+              list.map( option => (
+                <MenuItem key={option.code} value={option.code}>
+                  {option.name_with_type}
+                </MenuItem>
+              ))
+            }
+        </TextField>
+    );
+};
+
+SelectAddress.propTypes = {
+    list : PropTypes.array,
+    valSelect : PropTypes.string,
+    action: PropTypes.func,
+    label: PropTypes.string,
+    disable: PropTypes.bool,
+    margin: PropTypes.string,
+    fullWidth: PropTypes.bool,
+    required: PropTypes.bool
+};
+
+export default SelectAddress;
