7a210136383ce912184c9e01921eee715de5fd36
LeHieu <Xavan912@gmail.com>
2020-10-29 14:17:52 +0700

    Update UsersTable.js
    Su dung slug o trong ham tien ich da dinh nghia

--- src/views/UserList/components/UsersTable/UsersTable.js
+++ src/views/UserList/components/UsersTable/UsersTable.js
@@ -26,7 +26,7 @@ import {
   IconButton
 } from '@material-ui/core';
 import { SearchInput, SelectInput } from 'components';
-import { getInitials } from 'helpers';
+import { getInitials, toSlug } from 'helpers';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import EditAttributesIcon from '@material-ui/icons/EditAttributes';
 
@@ -83,36 +83,6 @@ const lists = [
     label: 'Theo tên Z-A',
   },
 ];
-
-const to_slug = str => {
-
-    // Chuyển hết sang chữ thường
-    str = str.toLowerCase();     
- 
-    // xóa dấu
-    str = str.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, 'a');
-    str = str.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, 'e');
-    str = str.replace(/(ì|í|ị|ỉ|ĩ)/g, 'i');
-    str = str.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, 'o');
-    str = str.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, 'u');
-    str = str.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, 'y');
-    str = str.replace(/(đ)/g, 'd');
- 
-    // Xóa ký tự đặc biệt
-    str = str.replace(/([^0-9a-z-\s])/g, '');
- 
-    // Xóa khoảng trắng thay bằng ký tự -
-    str = str.replace(/(\s+)/g, '-');
- 
-    // xóa phần dự - ở đầu
-    str = str.replace(/^-+/g, '');
- 
-    // xóa phần dư - ở cuối
-    str = str.replace(/-+$/g, '');
- 
-    // return
-    return str;
-}
 
 const UsersTable = props => {
   const { className, mockData, dispathSelect, ...rest } = props;
@@ -192,8 +162,8 @@ const UsersTable = props => {
       case 3:
         // Theo ten A-Z
         usersBy.sort( (a, b) => {
-          let x = to_slug(a.name);
-          let y = to_slug(b.name);
+          let x = toSlug(a.name);
+          let y = toSlug(b.name);
           if(x < y) {
            return -1;
          }
@@ -210,8 +180,8 @@ const UsersTable = props => {
       case 4:
         // Theo ten Z-A
         usersBy.sort( (a, b) => {
-          let x = to_slug(a.name);
-          let y = to_slug(b.name);
+          let x = toSlug(a.name);
+          let y = toSlug(b.name);
           if(x < y) {
            return -1;
          }
