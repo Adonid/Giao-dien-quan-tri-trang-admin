cf17658f3483a394e74e456388e4eeaaab7fab69
LeHieu <Xavan912@gmail.com>
2020-10-30 09:49:00 +0700

    Update CommunesBelongToDistrict.js
    thay try catch bang then catch de bat loi chinh xac

--- src/redux/actions/CommunesBelongToDistrict/CommunesBelongToDistrict.js
+++ src/redux/actions/CommunesBelongToDistrict/CommunesBelongToDistrict.js
@@ -6,37 +6,36 @@ const CommunesBelongToDistrict = parentCode => async dispatch => {
 
     dispatch({type: COMMUNES_BELONGTO_DISTRICT});
 
-    try{
-        const res = await axios({
-            method: 'POST',
-            url: 'admin/',
-            headers: { Authorization: "Bearer " + ReadCookie()},
-            data: {
-                query: `{
-                    communes: communesBelongToDistrict(parent_code: "${parentCode}"){
-                        name_with_type
-                        code
-                    }
-                }`,
-                variables: {}
-            }
+    const res = await axios({
+        method: 'POST',
+        url: 'admin/',
+        headers: { Authorization: "Bearer " + ReadCookie()},
+        data: {
+            query: `{
+                communes: communesBelongToDistrict(parent_code: "${parentCode}"){
+                    name_with_type
+                    code
+                }
+            }`,
+            variables: {}
+        }
+        })
+        .then( res => {
+            dispatch( {
+                type: COMMUNES_BELONGTO_DISTRICT_SUCCESS,
+                payload: {
+                    communes: res.data.data.communes
+                }
             });
-        dispatch( {
-            type: COMMUNES_BELONGTO_DISTRICT_SUCCESS,
-            payload: {
-                communes: res.data.data.communes
-            }
+        })
+        .catch( error => {
+            dispatch( {
+                type: COMMUNES_BELONGTO_DISTRICT_ERROR,
+                payload: {
+                    message: "Đã có lỗi xảy ra. status: 500",
+                },
+            });
         });
-    }
-    catch(e){
-        dispatch( {
-            type: COMMUNES_BELONGTO_DISTRICT_ERROR,
-            payload: {
-                message: "Đã có lỗi xảy ra. status: 500",
-            },
-        });
-    }
-
 }
 
 export default CommunesBelongToDistrict;
