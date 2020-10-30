35b845995c56bbdf66fd29f033c148d815321257
LeHieu <Xavan912@gmail.com>
2020-10-30 09:48:53 +0700

    Update AdminDetail.js
    thay try catch bang then catch de bat loi chinh xac

--- src/redux/actions/AdminDetail/AdminDetail.js
+++ src/redux/actions/AdminDetail/AdminDetail.js
@@ -6,49 +6,48 @@ const AdminDetail = () => async dispatch => {
     
     dispatch({type: ADMIN_DETAIL});
 
-    try{
-        const res = await axios({
-            method: 'POST',
-            url: 'admin/',
-            headers: { Authorization: "Bearer " + ReadCookie()},
-            data: {
-                query: `{
-                    adminDetail: adminProfile{
-                        userName
-                        phoneNumber
-                        email
-                        address{
-                            province
-                            district
-                            commune
-                            street
-                        }
+    const res = await axios({
+        method: 'POST',
+        url: 'admin/',
+        headers: { Authorization: "Bearer " + ReadCookie()},
+        data: {
+            query: `{
+                adminDetail: adminProfile{
+                    userName
+                    phoneNumber
+                    email
+                    address{
+                        province
+                        district
+                        commune
+                        street
                     }
-                    provinces{
-                        name_with_type
-                        code
-                    }
-                }`,
-                variables: {}
-            }
+                }
+                provinces{
+                    name_with_type
+                    code
+                }
+            }`,
+            variables: {}
+        }
+        })
+        .then( res => {
+            dispatch( {
+                type: ADMIN_DETAIL_SUCCESS,
+                payload: {
+                    profileDetail: res.data.data.adminDetail,
+                    provinces: res.data.data.provinces
+                }
             });
-        dispatch( {
-            type: ADMIN_DETAIL_SUCCESS,
-            payload: {
-                profileDetail: res.data.data.adminDetail,
-                provinces: res.data.data.provinces
-            }
+        })
+        .catch( error => {
+            dispatch( {
+                type: ADMIN_DETAIL_ERROR,
+                payload: {
+                    message: "Đã có lỗi xảy ra. status: 500",
+                },
+            });
         });
-    }
-    catch(e){
-        dispatch( {
-            type: ADMIN_DETAIL_ERROR,
-            payload: {
-                message: "Đã có lỗi xảy ra. status: 500",
-            },
-        });
-    }
-
 }
 
 export default AdminDetail;
