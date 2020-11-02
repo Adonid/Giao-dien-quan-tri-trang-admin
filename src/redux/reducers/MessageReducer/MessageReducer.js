32f71339952267b8e4bcfbe4926f8443522b1561
LeHieu <Xavan912@gmail.com>
2020-11-02 17:25:28 +0700

    Update MessageReducer.js
    Khoa nguoi dung

--- src/redux/reducers/MessageReducer/MessageReducer.js
+++ src/redux/reducers/MessageReducer/MessageReducer.js
@@ -1,5 +1,8 @@
+import { LockUsers } from 'redux/actions';
 import { 
-    MESSAGE_MAIN, MESSAGE_MINI
+    MESSAGE_MAIN, MESSAGE_MINI,
+    OPEN_DIALOG_CONFIRM,
+    CLOSE_DIALOG_CONFIRM
  } from 'redux/constans';
 
 const dataMessage = {
@@ -11,6 +14,10 @@ const dataMessage = {
         type    : "info",
         content : "Hello Word"
     },
+    openConfirm: false,
+    loadingConfirm: false,
+    contentConfirm: {},
+    dataConfirm: [],
 }
 
 const MessageReducer = (state = dataMessage, action) => {
@@ -22,6 +29,14 @@ const MessageReducer = (state = dataMessage, action) => {
 
         case MESSAGE_MINI:
             return { ...state, miniMessage: { type: action.payload.type, content: action.payload.message } };
+
+        case OPEN_DIALOG_CONFIRM:
+            const data = action.dataConfirm;
+            const content = action.contentConfirm;
+            return { ...state, openConfirm: true, contentConfirm: content, dataConfirm: data };
+        
+        case CLOSE_DIALOG_CONFIRM:
+            return { ...state, openConfirm: false };
 
         default:
             return state
