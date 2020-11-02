ae2003f1e8264925801987681ec81568136f6d0d
LeHieu <Xavan912@gmail.com>
2020-11-02 10:13:55 +0700

    Update MannegerUserReducer.js
    Lay danh sach nguoi viet bai

--- src/redux/reducers/MannegerUserReducer/MannegerUserReducer.js
+++ src/redux/reducers/MannegerUserReducer/MannegerUserReducer.js
@@ -1,11 +1,15 @@
 import { 
     CREATE_USER_SUCCESS, CREATE_USER_ERROR, CREATE_USER, OPEN_FORM_ADD_USER, CLOSE_FORM_ADD_USER,
+    GET_ALL_USER_SUCCESS, GET_ALL_USER_ERROR, GET_ALL_USER,
  } from 'redux/constans';
 
 const dataMannegerUser = {
     openForm: false,
     loadingCreate: false,
     messageCreate:"",
+
+    users: [],
+    loadingPage: true
 }
 
 const MannegerUserReducer = (state = dataMannegerUser, action) => {
@@ -26,6 +30,15 @@ const MannegerUserReducer = (state = dataMannegerUser, action) => {
 
         case CREATE_USER_ERROR:
             return { ...state, loadingCreate: false, messageCreate: action.payload.message };
+        
+        case GET_ALL_USER:
+            return { ...state, loadingPage: true };
+
+        case GET_ALL_USER_SUCCESS:
+            return { ...state, loadingPage: false, users: action.payload.users };
+
+        case GET_ALL_USER_ERROR:
+            return { ...state, loadingPage: false };
 
         default:
             return state
