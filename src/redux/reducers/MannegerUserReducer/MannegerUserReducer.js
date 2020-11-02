9c449b985314bce4852a682ecd900565d2c98e1e
LeHieu <Xavan912@gmail.com>
2020-11-02 17:25:02 +0700

    Update MannegerUserReducer.js
    Khoa nguoi dung

--- src/redux/reducers/MannegerUserReducer/MannegerUserReducer.js
+++ src/redux/reducers/MannegerUserReducer/MannegerUserReducer.js
@@ -1,6 +1,7 @@
 import { 
     CREATE_USER_SUCCESS, CREATE_USER_ERROR, CREATE_USER, OPEN_FORM_ADD_USER, CLOSE_FORM_ADD_USER,
     GET_ALL_USER_SUCCESS, GET_ALL_USER_ERROR, GET_ALL_USER,
+    LOCK_USERS_SUCCESS, LOCK_USERS_ERROR, LOCK_USERS,
  } from 'redux/constans';
 
 const dataMannegerUser = {
@@ -15,6 +16,8 @@ const dataMannegerUser = {
 const MannegerUserReducer = (state = dataMannegerUser, action) => {
     
     switch (action.type) {
+
+        // THEM MOI 1 NGUOI DUNG
         
         case OPEN_FORM_ADD_USER:
             return { ...state, openForm: true };
@@ -30,6 +33,8 @@ const MannegerUserReducer = (state = dataMannegerUser, action) => {
 
         case CREATE_USER_ERROR:
             return { ...state, loadingCreate: false, messageCreate: action.payload.message };
+
+        // LAY DANH SACH TAT CA NGUOI DUNG
         
         case GET_ALL_USER:
             return { ...state, loadingPage: true };
@@ -39,6 +44,18 @@ const MannegerUserReducer = (state = dataMannegerUser, action) => {
 
         case GET_ALL_USER_ERROR:
             return { ...state, loadingPage: false };
+
+        // KHOA NGUOI DUNG
+
+        case LOCK_USERS:
+            return { ...state, loadingPage: true };
+
+        case LOCK_USERS_SUCCESS:
+            return { ...state, loadingPage: false, users: action.payload.users };
+
+        case LOCK_USERS_ERROR:
+            return { ...state, loadingPage: false };
+
 
         default:
             return state
