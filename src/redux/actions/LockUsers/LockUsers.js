b12777cfc6d24e31f2d86d755bcbd7d102e83232
LeHieu <Xavan912@gmail.com>
2020-11-03 09:29:12 +0700

    Update LockUsers.js
    Tu logout ra ung dung khi khong vuot qua xac thuc admin

--- src/redux/actions/LockUsers/LockUsers.js
+++ src/redux/actions/LockUsers/LockUsers.js
@@ -1,5 +1,8 @@
 import axios from 'common/Axios';
-import { LOCK_USERS_SUCCESS, LOCK_USERS_ERROR, LOCK_USERS } from 'redux/constans';
+import { LOCK_USERS_SUCCESS, LOCK_USERS_ERROR, LOCK_USERS,
+     LOGOUT_ADMIN,
+     MESSAGE_MINI
+} from 'redux/constans';
 import { ReadCookie } from 'common';
 
 const LockUsers = uids => async dispatch => {
@@ -21,6 +24,21 @@ const LockUsers = uids => async dispatch => {
             });
         })
         .catch( error => {
+            if(error.response.data.exit){
+                dispatch( {
+                    type: LOGOUT_ADMIN,
+                    payload: {
+                        logged: false
+                    },
+                });
+                dispatch( {
+                    type: MESSAGE_MINI,
+                    payload: {
+                        message: error.response.data.message,
+                        type: "warning"
+                    },
+                });
+            }
             dispatch( {
                 type: LOCK_USERS_ERROR,
                 payload: {
