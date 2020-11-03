cb04c10e5945ac576e44345db130bc78726d141c
LeHieu <Xavan912@gmail.com>
2020-11-03 09:29:15 +0700

    Update UpdateProfile.js
    Tu logout ra ung dung khi khong vuot qua xac thuc admin

--- src/redux/actions/UpdateProfile/UpdateProfile.js
+++ src/redux/actions/UpdateProfile/UpdateProfile.js
@@ -1,12 +1,15 @@
 import axios from 'common/Axios';
-import { UPDATE_PROFILE_SUCCESS, UPDATE_PROFILE_ERROR, UPDATE_PROFILE } from 'redux/constans';
+import { UPDATE_PROFILE_SUCCESS, UPDATE_PROFILE_ERROR, UPDATE_PROFILE, 
+    LOGOUT_ADMIN,
+    MESSAGE_MINI
+} from 'redux/constans';
 import { ReadCookie } from 'common';
 
 const UpdateProfile = newProfile => async dispatch => {
     
     dispatch({type: UPDATE_PROFILE});
 
-    const res = await axios({
+    await axios({
         method: 'POST',
         url: 'admin/',
         headers: { Authorization: "Bearer " + ReadCookie()},
@@ -31,6 +34,21 @@ const UpdateProfile = newProfile => async dispatch => {
             });
         })
         .catch( error => {
+            if(error.response.data.exit){
+                dispatch( {
+                    type: LOGOUT_ADMIN,
+                    payload: {
+                        logged: false
+                    },
+                });
+                dispatch( {
+                    type: MESSAGE_MINI,
+                    payload: {
+                        message: error.response.data.message,
+                        type: "warning"
+                    },
+                });
+            }
             dispatch( {
                 type: UPDATE_PROFILE_ERROR,
                 payload: {
