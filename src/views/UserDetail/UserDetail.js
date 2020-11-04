8da46355e208e31fd36d6e9f669048de98d6f5d0
LeHieu <Xavan912@gmail.com>
2020-11-04 15:02:54 +0700

    Update UserDetail.js
    Toi uu cac dong code da duoc thay the

--- src/views/UserDetail/UserDetail.js
+++ src/views/UserDetail/UserDetail.js
@@ -185,10 +185,6 @@ const UserDetail = props => {
 
   const [ contextNotify, setContextNotify ] = useState('Nothing');
 
-  const [ openBlock, setOpenBlock ] = useState(false);
-  const [ openOpen, setOpenOpen ] = useState(false);
-  const [ openDistroy, setOpenDistroy ] = useState(false);
-
   const listSendActions = lists;
 
   useEffect( () => {
@@ -227,12 +223,6 @@ const UserDetail = props => {
     // Gui xong roi
     typeSend===2?setContextNotify(''):null;
   }
-
-  const handleBlocking = () => props.closeAccount({id: props.userinfo, name: props.userinfo.name});
-
-  const handleOpening = () => props.openAccount({id: props.userinfo, name: props.userinfo.name});
-
-  const handleDistroyUser0 = () => props.distroyAccount({id: props.userinfo, name: props.userinfo.name});
 
   const handleUnlockUser = () => {
     const contentConfirm = {
@@ -608,9 +598,6 @@ const UserDetail = props => {
           </Grid>
         </Box>
       </div>
-      <ConfirmDialog action={ handleBlocking } openDialog={openBlock} content={{type:'block', title:'Đóng tài khoản người dùng', note:`Tài khoản người dùng ${ props.userinfo.name } sẽ bị vô hiệu hóa cho đến khi bạn cho phép kích hoạt trở lại. Đóng tài khoản?`}} />
-      <ConfirmDialog action={ handleOpening } openDialog={openOpen} content={{type:'open', title:'Mở khóa tài khoản người dùng', note:`Tài khoản người dùng ${ props.userinfo.name } sẽ được phép hoạt động trở lại. Mở khóa cho tài khoản?`}} />
-      <ConfirmDialog action={ handleDistroyUser0 } openDialog={openDistroy} content={{type:'delete', title:'Xóa vĩnh viễn tài khoản người dùng', note:`Tài khoản ${ props.userinfo.name } sẽ bị xóa hoàn toàn trên hệ thống, thực thi sẽ không khôi phục được. Bạn có chắc?`}}/>
     </div>
   );
 };
