91aef371c9d4f80b6f039b74926d5f9626fc1a88
LeHieu <Xavan912@gmail.com>
2020-11-06 17:27:25 +0700

    Update MessageReducer.js
    Dong mo upload anh
    Thuc hien truyen tham so de uploa anh

--- src/redux/reducers/MessageReducer/MessageReducer.js
+++ src/redux/reducers/MessageReducer/MessageReducer.js
@@ -2,7 +2,10 @@ import {
     MESSAGE_MAIN, MESSAGE_MINI,
     OPEN_DIALOG_CONFIRM,
     CLOSE_DIALOG_CONFIRM,
-    LOADING_CONFIRM
+    LOADING_CONFIRM,
+    OPEN_DIALOG_UPLOAD_IMG,
+    CLOSE_DIALOG_UPLOAD_IMG,
+    PROCESS_DIALOG_UPLOAD_IMG,
  } from 'redux/constans';
 
 const dataMessage = {
@@ -10,36 +13,55 @@ const dataMessage = {
         type    : "info",
         content : "Hello Word"
     },
+
     miniMessage: {
         type    : "info",
         content : "Hello Word"
     },
+
     openConfirm: false,
     loadingConfirm: false,
     contentConfirm: {},
     dataConfirm: [],
+
+    openUploadImg: false,
+    loadingUpload: false,
+    contentUploadImg: {
+        imageInit:'',
+        titleName:''
+    },
 }
 
 const MessageReducer = (state = dataMessage, action) => {
     
     switch (action.type) {
-        
+        // Mo thong bao giao dien chinh
         case MESSAGE_MAIN:
             return { ...state, mainMessage: { type: action.payload.type, content: action.payload.message } };
-
+        // Mo thong bao giao dien phu
         case MESSAGE_MINI:
             return { ...state, miniMessage: { type: action.payload.type, content: action.payload.message } };
-
+        // Mo cua so xac nhan
         case OPEN_DIALOG_CONFIRM:
             const data = action.dataConfirm;
             const content = action.contentConfirm;
             return { ...state, openConfirm: true, contentConfirm: content, dataConfirm: data };
-        
+        // Trang thai loading xac nhan
         case LOADING_CONFIRM:
             return { ...state, loadingConfirm: true };
-        
+        // Dong cua so xac nhan
         case CLOSE_DIALOG_CONFIRM:
             return { ...state, openConfirm: false, loadingConfirm: false };
+        // Mo cua so upload anh
+        case OPEN_DIALOG_UPLOAD_IMG:
+            const contentUpload = action.content
+            return { ...state, openUploadImg: true, contentUploadImg: contentUpload };
+        // Xu ly upload anh
+        case PROCESS_DIALOG_UPLOAD_IMG:
+            return { ...state, loadingUpload: action.payload.loading };
+        // Dong cua so upload anh
+        case CLOSE_DIALOG_UPLOAD_IMG:
+            return { ...state, openUploadImg: false };
 
         default:
             return state
