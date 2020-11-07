7778ebef43a6f199b2c50ff894c08682dc5750b3
LeHieu <Xavan912@gmail.com>
2020-11-07 22:09:26 +0700

    Create UpdateUser.js
    Cap nhat user

---
+++ src/redux/actions/UpdateUser/UpdateUser.js
@@ -0,0 +1,72 @@
+import axios from 'common/Axios';
+import { 
+    UPDATE_USER,
+    UPDATE_USER_SUCCESS,
+    UPDATE_USER_ERROR,
+    LOGOUT_ADMIN,
+    MESSAGE_MINI,
+    MESSAGE_MAIN,
+} from 'redux/constans';
+import { ReadCookie } from 'common';
+
+const UpdateUser = dataUpdate => async dispatch => {
+    
+    dispatch({type: UPDATE_USER});
+
+    await axios({
+        method: 'POST',
+        url: 'admin/update-account-user',
+        headers: { Authorization: "Bearer " + ReadCookie()},
+        data: dataUpdate
+        })
+        .then( res => {
+            dispatch( {
+                type: UPDATE_USER_SUCCESS,
+                payload: {
+                    message: res.data.message,
+                    token: res.data.tokenImg,
+                    loading: false
+                }
+            });
+            dispatch( {
+                type: MESSAGE_MAIN,
+                payload: {
+                    message: res.data.message,
+                    type: "success"
+                }
+            });
+        })
+        .catch( error => {
+            if(typeof(error.response.data.exit) === 'boolean'){
+                dispatch( {
+                    type: LOGOUT_ADMIN,
+                    payload: {
+                        logged: false
+                    },
+                });
+                dispatch( {
+                    type: MESSAGE_MINI,
+                    payload: {
+                        message: error.response.data.message,
+                        type: "warning"
+                    },
+                });
+            }
+            dispatch({
+                type: UPDATE_USER_ERROR,
+                payload: {
+                    loading: false
+                }
+            });
+            dispatch( {
+                type: MESSAGE_MAIN,
+                payload: {
+                    message: error.response.data.message,
+                    type: "error"
+                }
+            });
+        });
+
+}
+
+export default UpdateUser;
