c5ecd36e868aaf189c0470ad2cfd3139534f2e79
LeHieu <Xavan912@gmail.com>
2020-11-07 22:07:50 +0700

    Update index.js
    Thay doi cach bat loi

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,3 +2,4 @@ export { default as chartjs } from './chartjs';
 export { default as getInitials } from './getInitials';
 export { default as filterPost } from './filterPost';
 export { default as toSlug } from './toSlug';
+export { default as getAvatarUrl } from './getAvatarUrl';
