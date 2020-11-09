99e33962f5fd65fad0ce1e76b60b71c24c8873c2
LeHieu <Xavan912@gmail.com>
2020-11-09 13:24:48 +0700

    Update index.js
    Xoa reducer ko can nua

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,6 @@
 export { default as NotifysReducers  } from './NotifysReducers';
 export { default as UsersListReducers  } from './UsersListReducers';
 export { default as UserDetailReducer  } from './UserDetailReducer';
-export { default as UserEditorReducer  } from './UserEditorReducer';
 export { default as ChatsDataReducer  } from './ChatsDataReducer';
 export { default as ProductsListReducer  } from './ProductsListReducer';
 export { default as ManipulationPostReducer  } from './ManipulationPostReducer';
