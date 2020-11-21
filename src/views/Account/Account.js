b38894e20dc224efff8cb30b09c4c44961e00819
LeHieu <Xavan912@gmail.com>
2020-11-22 06:51:42 +0700

    Update Account.js
    Fetch du lieu tu component cha

--- src/views/Account/Account.js
+++ src/views/Account/Account.js
@@ -1,20 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
-import { Grid } from '@material-ui/core';
-
+import { 
+  Grid,
+  Card,
+  CardContent
+ } from '@material-ui/core';
+import Skeleton from '@material-ui/lab/Skeleton';
 import { AccountProfile, AccountDetails } from './components';
 import { connect } from 'react-redux';
 
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(4)
+  },
+  skeletonDetail: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  skeletonText: {
+    position: 'relative',
+    width: '100%',
   }
 }));
 
 const Account = props => {
-  const { profile, } = props;
+  const { profile, loading } = props;
   const classes = useStyles();
+
+  if(loading){
+    return (
+      <Card
+        {...rest}
+        className={clsx(classes.root, className)}
+      >
+        <CardContent>
+          <div className={classes.skeletonDetail}>
+            <div className={ classes.skeletonText}>
+              <Skeleton animation="wave" style={{ marginBottom: 10 }} width="60%" />
+              <Skeleton animation="wave" style={{ marginBottom: 5 }} height={10} width="80%" />
+              <Skeleton animation="wave" height={10} width="60%" />
+            </div>
+            <div>
+              <Skeleton animation="wave" variant="circle" width={80} height={80} />
+            </div>
+          </div>
+        </CardContent>
+        <Divider />
+        <CardActions>
+            <Skeleton animation="wave" height={10} width="60%"/>
+        </CardActions>
+      </Card>
+    );
+  }
 
   return (
     <div className={classes.root}>
@@ -50,10 +88,11 @@ Account.propTypes = {
 };
 
 const mapStateToProps = state => ({
-  profile: state.dataUserProfile
+  profile: state.dataUserProfile.profile,
+  loading: state.dataUserProfile.loading,
 });
 
 
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Account);
+export default connect(mapStateToProps, null)(Account);
