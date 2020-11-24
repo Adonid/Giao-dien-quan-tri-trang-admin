e8b16795f12acc1f66c542d91e79cdfdd4fa6f0b
LeHieu <Xavan912@gmail.com>
2020-11-24 11:00:15 +0700

    Create GetMyProfile.js

---
+++ src/redux/actions/GetMyProfile/GetMyProfile.js
@@ -0,0 +1,55 @@
+import axios from 'common/Axios';
+import { 
+    GET_MY_PROFILE_SUCCESS, GET_MY_PROFILE,
+    LOGOUT_ADMIN,
+    MESSAGE_MINI,
+    MESSAGE_MAIN
+} from 'redux/constans';
+import { ReadCookie } from 'common';
+
+const GetMyProfile = () => async dispatch => {
+    
+    dispatch({type: GET_MY_PROFILE});
+
+    await axios({
+        method: 'GET',
+        url: 'admin/get-profile-user',
+        headers: { Authorization: "Bearer " + ReadCookie()},
+        })
+        .then( res => {
+            dispatch( {
+                type: GET_MY_PROFILE_SUCCESS,
+                payload: {
+                    profile: res.data.profile,
+                    profileExtend: res.data.profileExtend,
+                }
+            });
+        })
+        .catch( error => {
+            if(typeof(error.response.data.exit) === 'boolean'){
+                dispatch( {
+                    type: LOGOUT_ADMIN,
+                    payload: {
+                        logged: false
+                    },
+                });
+                dispatch( {
+                    type: MESSAGE_MINI,
+                    payload: {
+                        message: error.response.data.message,
+                        type: "warning"
+                    },
+                });
+            }
+            dispatch( {
+                type: MESSAGE_MAIN,
+                payload: {
+                    message: error.response.data.message,
+                    type: "error"
+                },
+            });
+    });
+
+}
+
+export default GetMyProfile;
